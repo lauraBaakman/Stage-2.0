df628020f8491cca2ab5fda3e8f6a219aa237617
Laura Baakman <laurabaakman@gmail.com>
2016-11-24 12:06:50 +0100

    Set up of unit test directory.

---
+++ src/tests/kde/kernels/test_gaussian.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestGaussian(TestCase):
+    def test_center(self):
+        self.fail()
+
+    def test_center(self):
+        self.fail()
+
+    def test_shape(self):
+        self.fail()
+
+    def test_shape(self):
+        self.fail()
+
+    def test__update_kernel(self):
+        self.fail()
+
+    def test_evaluate(self):
+        self.fail()
