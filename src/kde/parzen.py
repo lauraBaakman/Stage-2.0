6342275afe66ecd7b9922d992dfe7feaa9ce2422
Laura Baakman <laurabaakman@gmail.com>
2016-11-24 14:45:18 +0100

    Fixed the naive implementation

--- src/kde/parzen.py
+++ src/kde/parzen.py
@@ -12,17 +12,23 @@ class Parzen:
         self._window_width = window_width
         self._dimension = dimension
 
-    def estimate_python(self, data, patterns_to_estimate=None):
-        self._kernel.center = np.zeros((self._dimension))
+    def estimate_python(self, x_is, x_s=None):
+        if x_s is None:
+            x_s = x_is
+        (n, _) = x_is.shape
+
+        self._kernel.center = np.zeros(self._dimension)
         self._kernel.shape = np.identity(self._dimension)
-        if patterns_to_estimate is None:
-            patterns_to_estimate = data
-        (n, _) = data.shape
-        factor = 1 / (n * (math.pow(self._window_width, self._dimension)))
-        for x in patterns_to_estimate:
-            sum = 0
-            for x_i in data:
-                sum += self._kernel.evaluate(1/self._window_width * (x - x_i))
+
+        factor = 1 / (n * math.pow(self._window_width, self._dimension))
+        (n_x_i, _) = x_s.shape
+        densities = np.empty(n_x_i)
+        for idx, x in enumerate(x_s):
+            bump_sum = 0
+            for x_i in x_is:
+                bump_sum += self._kernel.evaluate((x - x_i) / self._window_width)
+            densities[idx] = factor * bump_sum
+        return densities
 
     def estimate_python_vectorized(self, data, patterns_to_estimate=None):
         if patterns_to_estimate is None:
@@ -36,4 +42,4 @@ def benchmark_python(n=1000, dimension=3):
     kernel_shape = window_width * window_width * np.identity(dimension)
     kernel = kernels.Gaussian(covariance_matrix=kernel_shape)
     estimator = Parzen(window_width=window_width, dimension=dimension, kernel=kernel)
-    densities = estimator.estimate_python(data=patterns)
+    densities = estimator.estimate_python(x_is=patterns)
