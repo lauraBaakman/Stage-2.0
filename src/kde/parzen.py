ebb60e6735e2f73f77944bfd34baddc39c7c6604
Laura Baakman <laurabaakman@gmail.com>
2016-11-24 14:09:47 +0100

    Improving implementation

--- src/kde/parzen.py
+++ src/kde/parzen.py
@@ -1,26 +1,30 @@
+import math
+
 import numpy as np
+
 import kde.kernels as kernels
 
 
 class Parzen:
 
-    def __init__(self, kernel):
+    def __init__(self, window_width, dimension, kernel):
         self._kernel = kernel
+        self._window_width = window_width
+        self._dimension = dimension
 
     def estimate_python(self, data, patterns_to_estimate=None):
+        self._kernel.center = np.zeros((self._dimension))
+        self._kernel.shape = np.identity(self._dimension)
         if patterns_to_estimate is None:
             patterns_to_estimate = data
-        densities = np.apply_along_axis(
-            self._estimate_python_pattern_density, 1, patterns_to_estimate, data)
-        return densities
+        (n, _) = data.shape
+        factor = 1 / (n * (math.pow(self._window_width, self._dimension)))
+        for x in patterns_to_estimate:
+            sum = 0
+            for x_i in data:
+                sum += self._kernel.evaluate(1/self._window_width * (x - x_i))
 
-    def _estimate_python_pattern_density(self, x, xi_s):
-        self._kernel.center = x
-        terms = np.apply_along_axis(lambda x_i: self._kernel.evaluate(x_i), 1, xi_s)
-        density = terms.sum()
-        return density
-
-    def estimate_pyton_vectorized(self, data, patterns_to_estimate=None):
+    def estimate_python_vectorized(self, data, patterns_to_estimate=None):
         if patterns_to_estimate is None:
             patterns_to_estimate = data
         return None
@@ -28,6 +32,8 @@ class Parzen:
 
 def benchmark_python(n=1000, dimension=3):
     patterns = np.random.randn(n, dimension)
-    kernel = kernels.Gaussian(covariance_matrix=0.5 * np.identity(3))
-    estimator = Parzen(kernel=kernel)
+    window_width = 1 / np.sqrt(n)
+    kernel_shape = window_width * window_width * np.identity(dimension)
+    kernel = kernels.Gaussian(covariance_matrix=kernel_shape)
+    estimator = Parzen(window_width=window_width, dimension=dimension, kernel=kernel)
     densities = estimator.estimate_python(data=patterns)
