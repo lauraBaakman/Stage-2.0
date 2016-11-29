8518a421ba529273e7682603ac688ed9d5b3406b
Laura Baakman <laurabaakman@gmail.com>
2016-11-29 15:20:18 +0100

    Added Epanechnikov kernel.

--- src/kde/kernels/__init__.py
+++ src/kde/kernels/__init__.py
@@ -1,2 +1,2 @@
 from kde.kernels.gaussian import Gaussian
-from kde.kernels.standardgaussian import StandardGaussian
+from kde.kernels.epanecnikov import Epanechnikov
---
+++ src/kde/kernels/epanecnikov.py
@@ -0,0 +1,2 @@
+class Epanechnikov:
+    pass
