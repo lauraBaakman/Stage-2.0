b33ba240f349d0fef3d8fc49db88e07984b67b37
Laura Baakman <laurabaakman@gmail.com>
2016-11-29 16:50:49 +0100

    Implemented the Epanechnikov kernel

--- src/kde/kernels/epanechnikov.py
+++ src/kde/kernels/epanechnikov.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+import numpy as np
 
 # http://stackoverflow.com/questions/17177061/epanechnikov-multivariate-density
 class Multivariate(Enum):
@@ -7,25 +8,34 @@ class Multivariate(Enum):
     norm = 2
 
 
+def _univariate(x):
+    return np.squeeze(3/4 * (1 - np.square(x)) * (abs(x) <= 1))
+
+
+def _multivariate_multiplicative(x):
+    univariate_results = _univariate(x)
+    try:
+        value = np.prod(univariate_results, axis=1)
+    except ValueError:
+        value = np.prod(univariate_results)
+    return value
+
+
+def _multivariate_norm(x):
+    univariate_results = _univariate(x)
+    try:
+        value = np.linalg.norm(univariate_results, axis=1)
+    except ValueError:
+        value = np.linalg.norm(univariate_results)
+    return value
+
+
 class Epanechnikov:
 
     def __init__(self, dimension, multivariate_approach=Multivariate.multiplication):
-        self._dimension = dimension
         if dimension is 1:
-            self.evaluate = self._univariate
+            self.evaluate = _univariate
         elif dimension > 1 and multivariate_approach is Multivariate.multiplication:
-            self.evaluate = self._multivariate_multiplicative
+            self.evaluate = _multivariate_multiplicative
         elif dimension > 1 and multivariate_approach is Multivariate.norm:
-            self.evaluate = self._multivariate_norm
-
-    def _univariate(self, x):
-        print("Univariate")
-        raise NotImplementedError()
-
-    def _multivariate_multiplicative(self, x):
-        print("Multiplicative")
-        raise NotImplementedError()
-
-    def _multivariate_norm(self, x):
-        print("Norm")
-        raise NotImplementedError
+            self.evaluate = _multivariate_norm
