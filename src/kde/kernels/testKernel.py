599d97f776103f17c8832dfc0cc3c126947c1581
Laura Baakman <laurabaakman@gmail.com>
2016-12-06 13:18:09 +0100

    It helps if the test kernel returns only positive values.

--- src/kde/kernels/testKernel.py
+++ src/kde/kernels/testKernel.py
@@ -3,4 +3,4 @@ import  numpy as np
 class TestKernel:
 
     def evaluate(self, x):
-        return np.mean(x, axis=1)
+        return np.abs(np.mean(x, axis=1))
--- src/kde/kernels/tests/test_testKernel.py
+++ src/kde/kernels/tests/test_testKernel.py
@@ -6,9 +6,14 @@ from kde.kernels.testKernel import TestKernel
 
 
 class TestTestKernel(TestCase):
-
-    def test_evaluate(self):
+    def test_evaluate_1(self):
         x = np.array([[2, 3], [3, 4], [4, 5]])
+        actual = TestKernel().evaluate(x)
+        expected = np.array([2.5, 3.5, 4.5])
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_evaluate_2(self):
+        x = np.array([[-2, -3], [-3, -4], [-4, -5]])
         actual = TestKernel().evaluate(x)
         expected = np.array([2.5, 3.5, 4.5])
         np.testing.assert_array_equal(actual, expected)
