5a8e3f763f5aad52b36fea465992ed4c0dd58d5b
Laura Baakman <laurabaakman@gmail.com>
2017-01-09 14:22:59 +0100

    Follow C naming conventions in C code.

--- src/kde/kernels/densityFunctions.c
+++ src/kde/kernels/densityFunctions.c
@@ -6,9 +6,9 @@
 
 double standard_gaussian(double* pattern, int pattern_dimensionality){
     double factor = pow(2 * M_PI, - 1 * pattern_dimensionality * 0.5);
-    double dot_product = 0.0;
+    double dotProduct = 0.0;
     for(int i = 0; i < pattern_dimensionality; i++) {
-        dot_product += pattern[i] * pattern[i];
+        dotProduct += pattern[i] * pattern[i];
     }
-    return factor * exp(-0.5 * dot_product);
+    return factor * exp(-0.5 * dotProduct);
 }
