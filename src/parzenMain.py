06f812fdbc982c14130e821a30a3bd20ac3399a4
Laura Baakman <laurabaakman@gmail.com>
2017-01-09 17:06:00 +0100

    Pass patterns and datapoints to the C function.

--- src/parzenMain.py
+++ src/parzenMain.py
@@ -4,8 +4,9 @@ import kde._kde as _kde
 
 if __name__ == '__main__':
     patterns = np.array([[0.5, 0.5], [0.5, 1.5], [0.5, 0.7]])
+    datapoints = np.array([[0.4, 0.4], [0.4, 1.4], [0.4, 0.6], [0.3, 0.4]])
     (num_patterns, _) = patterns.shape
     densities = np.empty(num_patterns, dtype=float)
-    _kde.parzen_multi_pattern(patterns, densities)
+    _kde.parzen_multi_pattern(patterns, datapoints, densities)
 
     print(densities)
