3e96bf29f4bea80313fec55097fa8968301ba8ae
Laura Baakman <laurabaakman@gmail.com>
2017-01-09 15:40:58 +0100

    Script to compare performances.

--- src/comparePerformance.py
+++ src/comparePerformance.py
@@ -4,17 +4,19 @@ import numpy as np
 
 
 def test_1():
-    from kde.kernels.standardGaussian_new import StandardGaussian_New as the_class
-    gaussian = the_class(dimension=3)
-    matrix = np.random.rand(10000, 3)
-    gaussian.evaluate(matrix)
+    # from kde.kernels.standardGaussian_new import StandardGaussian_New as the_class
+    # gaussian = the_class(dimension=3)
+    # matrix = np.random.rand(10000, 3)
+    # gaussian.evaluate(matrix)
+    pass
 
 
 def test_2():
-    from kde.kernels.standardGaussian import StandardGaussian as the_class
-    gaussian = the_class(dimension=3)
-    matrix = np.random.rand(10000, 3)
-    gaussian.evaluate(matrix)
+    # from kde.kernels.standardGaussian import StandardGaussian as the_class
+    # gaussian = the_class(dimension=3)
+    # matrix = np.random.rand(10000, 3)
+    # gaussian.evaluate(matrix)
+    pass
 
 if __name__ == '__main__':
     print(timeit.timeit(
