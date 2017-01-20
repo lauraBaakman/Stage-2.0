eb49a3a4239b404d5823267313231d6bc79ba0b8
Laura Baakman <laurabaakman@gmail.com>
2017-01-20 17:21:19 +0100

    Compare performance between different parzen implemenations.

--- src/comparePerformance.py
+++ src/comparePerformance.py
@@ -4,19 +4,31 @@ import numpy as np
 
 
 def test_1():
-    # from kde.kernels.standardGaussian_new import StandardGaussian_New as the_class
-    # gaussian = the_class(dimension=3)
-    # matrix = np.random.rand(10000, 3)
-    # gaussian.evaluate(matrix)
-    pass
+    dimension = 5
+    window_width = 0.25
+
+    import kde._kde as kde
+    patterns = np.random.rand(5000, dimension)
+    data_points = np.random.rand(500, dimension)
+    (num_patterns, dimension) = patterns.shape
+    densities = np.empty(num_patterns, dtype=float)
+    kde.parzen_standard_gaussian(patterns, data_points, window_width, densities)
 
 
 def test_2():
-    # from kde.kernels.standardGaussian import StandardGaussian as the_class
-    # gaussian = the_class(dimension=3)
-    # matrix = np.random.rand(10000, 3)
-    # gaussian.evaluate(matrix)
-    pass
+    dimension = 5
+    window_width = 0.25
+
+    import kde
+    patterns = np.random.rand(5000, dimension)
+    data_points = np.random.rand(500, dimension)
+    (num_patterns, dimension) = patterns.shape
+
+    kernel_shape = window_width * window_width * dimension
+    kernel = kde.kernels.Gaussian(covariance_matrix=kernel_shape)
+
+    estimator = kde.Parzen(dimension, window_width, kernel)
+    densities = estimator.estimate(xi_s=data_points, x_s=patterns)
 
 if __name__ == '__main__':
     print(timeit.timeit(
