bced5660abc37d056dcf6d295fef77f04b274803
Laura Baakman <laurabaakman@gmail.com>
2017-01-26 16:30:56 +0100

    Files for MBE

---
+++ src/kde/mbe.c
@@ -0,0 +1,5 @@
+//
+// Created by Laura Baakman on 26/01/2017.
+//
+
+#include "mbe.h"
---
+++ src/kde/mbe.h
@@ -0,0 +1,8 @@
+//
+// Created by Laura Baakman on 26/01/2017.
+//
+
+#ifndef KERNELS_MBE_H
+#define KERNELS_MBE_H
+
+#endif //KERNELS_MBE_H
