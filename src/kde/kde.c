e3aa9e3b98bd1259d86573ef1e360db6ae2a681c
Laura Baakman <laurabaakman@gmail.com>
2017-01-26 16:53:51 +0100

    Added support for the Breiman estimator, the ugly way.

--- src/kde/kde.c
+++ src/kde/kde.c
@@ -74,6 +74,41 @@ static PyObject * kdeParzenEpanechnikov(PyObject *self, PyObject *args){
     return Py_None;
 }
 
+static char kde_breiman_epanechnikov_docstring[] = "Estimate the densities with Parzen with the Epanechnikov kernel.";
+static PyObject * kdeBreimanEpanechnikov(PyObject *self, PyObject *args){
+    PyObject* inPatterns = NULL;
+    PyObject* inDataPoints = NULL;
+    PyObject* outDensities = NULL;
+
+    double windowWidth;
+
+    if (!PyArg_ParseTuple(args, "OOdO",
+                          &inPatterns,
+                          &inDataPoints,
+                          &windowWidth,
+                          &outDensities)) return NULL;
+
+    Array patterns = pyObjectToArray(inPatterns, NPY_ARRAY_IN_ARRAY);
+    Array dataPoints = pyObjectToArray(inDataPoints, NPY_ARRAY_IN_ARRAY);
+    Array densities = pyObjectToArray(outDensities, NPY_ARRAY_OUT_ARRAY);
+
+    double parzenFactor = 1.0 / (dataPoints.length * pow(windowWidth, patterns.dimensionality));
+    double epanechnikovFactor = epanechnikovDenominator(dataPoints.dimensionality);
+
+    double* current_pattern = patterns.data;
+
+    for(int j = 0;
+        j < patterns.length;
+        j++, current_pattern += patterns.stride)
+    {
+        densities.data[j] = parzen_epanechnikov(current_pattern, &dataPoints, windowWidth, parzenFactor, epanechnikovFactor);
+    }
+
+    /* Create return object */
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
 Array pyObjectToArray(PyObject *pythonObject, int requirements){
     PyArrayObject* arrayObject = NULL;
     arrayObject = (PyArrayObject *)PyArray_FROM_OTF(pythonObject, NPY_DOUBLE, requirements);
@@ -90,6 +125,7 @@ Array pyObjectToArray(PyObject *pythonObject, int requirements){
 static PyMethodDef method_table[] = {
         {"parzen_standard_gaussian",    kdeParzenStandardGaussian,  METH_VARARGS,   kde_parzen_standardGaussian_docstring},
         {"parzen_epanechnikov",         kdeParzenEpanechnikov,      METH_VARARGS,   kde_parzen_epanechnikov_docstring},
+        {"breiman_epanechnikov",        kdeBreimanEpanechnikov,     METH_VARARGS,   kde_breiman_epanechnikov_docstring},
         /* Sentinel */
         {NULL,                              NULL,                                   0,              NULL}
 };
