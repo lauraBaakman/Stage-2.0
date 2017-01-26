13f87e9f845075c6657967787d28d34ce13b4262
Laura Baakman <laurabaakman@gmail.com>
2017-01-26 10:22:20 +0100

    The parzen class is now a wrapper for the C implementation.

--- src/kde/parzen.py
+++ src/kde/parzen.py
@@ -2,21 +2,20 @@ import math
 
 import numpy as np
 
-import kde.kernels as kernels
+import kde._kde as kde
+from kde.estimator import Estimator
 
 
-class Parzen:
-    """Implementation of the Parzen Estimator.
+class Parzen(Estimator):
+    """
+    Wrapper for the C implementation of Parzen density estimation with a Gaussian kernel.
     """
 
-    def __init__(self, dimension, window_width, kernel=None):
-        """ Init method of the Parzen Estimator.
-        :param dimension: (int) The dimension of the data points of which the density is estimated.
+    def __init__(self, window_width, *args, **kwargs):
+        """ Init method of the Parzen Estimator with a Gaussian kernel.
         :param window_width: (int) The window width to use.
-        :param kernel: (kernel, optional) The kernel to use for the final density estimate, defaults to Gaussian.
         """
-        self._dimension = dimension
-        self._kernel = kernel or kernels.Gaussian()
+        super(Estimator, self).__init__()
         self._window_width = window_width
 
     def estimate(self, xi_s, x_s=None):
@@ -25,47 +24,77 @@ class Parzen:
         :param x_s: (array like, optional) The data points to use to estimate the density. Defaults to xi_s.
         :return: The estimated densities of x_s.
         """
+
         if x_s is None:
             x_s = xi_s
-        estimator = _ParzenEstimator(
-            xi_s=xi_s, x_s=x_s,
-            dimension=self._dimension, kernel=self._kernel, window_width=self._window_width)
-        return estimator.estimate()
-
-    def __repr__(self):
-        return "%s(%r)" % (self.__class__, self.__dict__)
-
-
-class _ParzenEstimator:
-
-    def __init__(self, xi_s, x_s, dimension, kernel, window_width):
-        self._xi_s = xi_s
-        self._x_s = x_s
-        self._dimension = dimension
-        self._kernel = kernel
-        self._window_width = window_width
-
-    @property
-    def num_xi_s(self):
-        (n, _) = self._xi_s.shape
-        return n
-
-    @property
-    def num_x_s(self):
-        (n, _) = self._x_s.shape
-        return n
-
-    def estimate(self):
-        self._kernel.center = np.zeros(self._dimension)
-        self._kernel.shape = np.identity(self._dimension)
-
-        densities = np.empty(self.num_x_s)
-        factor = 1 / (self.num_xi_s * math.pow(self._window_width, self._dimension))
-        for idx, x in enumerate(self._x_s):
-            densities[idx] = self._estimate_pattern(x, factor)
+        self._validate_data(x_s, xi_s)
+        (num_patterns, _) = xi_s.shape
+        densities = np.empty(num_patterns, dtype=float)
+        kde.parzen_standard_gaussian(x_s, xi_s, self._window_width, densities)
         return densities
 
-    def _estimate_pattern(self, x, factor):
-        terms = self._kernel.evaluate((x - self._xi_s)/self._window_width)
-        density = factor * terms.sum()
-        return density
+
+# class _ParzenInPython:
+#     """Implementation of the Parzen Estimator.
+#     """
+#
+#     def __init__(self, dimension, window_width, kernel=None):
+#         """ Init method of the Parzen Estimator.
+#         :param dimension: (int) The dimension of the data points of which the density is estimated.
+#         :param window_width: (int) The window width to use.
+#         :param kernel: (kernel, optional) The kernel to use for the final density estimate, defaults to Gaussian.
+#         """
+#         self._dimension = dimension
+#         self._kernel = kernel or kernels.Gaussian()
+#         self._window_width = window_width
+#
+#     def estimate(self, xi_s, x_s=None):
+#         """Estimate the density of the points xi_s, use the points x_s to determine the density.
+#         :param xi_s: (array like) The data points to estimate the density for.
+#         :param x_s: (array like, optional) The data points to use to estimate the density. Defaults to xi_s.
+#         :return: The estimated densities of x_s.
+#         """
+#         if x_s is None:
+#             x_s = xi_s
+#         estimator = _ParzenEstimator(
+#             xi_s=xi_s, x_s=x_s,
+#             dimension=self._dimension, kernel=self._kernel, window_width=self._window_width)
+#         return estimator.estimate()
+#
+#     def __repr__(self):
+#         return "%s(%r)" % (self.__class__, self.__dict__)
+#
+#
+# class _ParzenEstimator:
+#
+#     def __init__(self, xi_s, x_s, dimension, kernel, window_width):
+#         self._xi_s = xi_s
+#         self._x_s = x_s
+#         self._dimension = dimension
+#         self._kernel = kernel
+#         self._window_width = window_width
+#
+#     @property
+#     def num_xi_s(self):
+#         (n, _) = self._xi_s.shape
+#         return n
+#
+#     @property
+#     def num_x_s(self):
+#         (n, _) = self._x_s.shape
+#         return n
+#
+#     def estimate(self):
+#         self._kernel.center = np.zeros(self._dimension)
+#         self._kernel.shape = np.identity(self._dimension)
+#
+#         densities = np.empty(self.num_x_s)
+#         factor = 1 / (self.num_xi_s * math.pow(self._window_width, self._dimension))
+#         for idx, x in enumerate(self._x_s):
+#             densities[idx] = self._estimate_pattern(x, factor)
+#         return densities
+#
+#     def _estimate_pattern(self, x, factor):
+#         terms = self._kernel.evaluate((x - self._xi_s)/self._window_width)
+#         density = factor * terms.sum()
+#         return density
--- src/kde/tests/test_parzen.py
+++ src/kde/tests/test_parzen.py
@@ -2,43 +2,27 @@ from unittest import TestCase
 
 import numpy as np
 
+import kde
+
 
 class TestParzen(TestCase):
 
     def setUp(self):
         super().setUp()
-        self.data = {
-            'patterns': np.array([
+        self._patterns = np.array([
                 [2, 3],
                 [1, 3],
                 [-2, 2]
-            ]),
-            'parzen_estimated_densities': np.array([
+            ])
+        self._parzen_estimated_densities = np.array([
                 0.591741568117374,
                 0.591741568117374,
                 0.589462752192205
-            ]),
-            'window_width': 0.3,
-            'dimension': 2
-        }
+            ])
+        self._window_width = 0.3
 
-    def test_estimate_python(self):
-        import kde
-        kernel_shape = self.data['window_width'] * self.data['window_width'] * np.identity(self.data['dimension'])
-        kernel = kde.kernels.Gaussian(covariance_matrix=kernel_shape)
-        estimator = kde.Parzen(
-            dimension=self.data['dimension'],
-            window_width=self.data['window_width'],
-            kernel=kernel
-        )
-        actual = estimator.estimate(xi_s=self.data['patterns'])
-        expected = self.data['parzen_estimated_densities']
-        np.testing.assert_array_almost_equal(actual, expected)
-
-    def test_estimate_C(self):
-        import kde._kde as kde
-        (num_patterns, dimension) = self.data['patterns'].shape
-        actual = np.empty(num_patterns, dtype=float)
-        kde.parzen_standard_gaussian(self.data['patterns'], self.data['patterns'], self.data['window_width'], actual)
-        expected = self.data['parzen_estimated_densities']
+    def test_estimate(self):
+        estimator = kde.Parzen(window_width=self._window_width)
+        actual = estimator.estimate(xi_s=self._patterns)
+        expected = self._parzen_estimated_densities
         np.testing.assert_array_almost_equal(actual, expected)
