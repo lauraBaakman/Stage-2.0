b445085681d99f12cba36fe9e37b301401bf330a
Laura Baakman <laurabaakman@gmail.com>
2017-01-26 10:24:52 +0100

    Failed test that threw an error.

--- src/kde/tests/test_modifiedBreiman.py
+++ src/kde/tests/test_modifiedBreiman.py
@@ -51,6 +51,7 @@ class TestModifiedBreimanEstimator(TestCase):
         np.testing.assert_array_almost_equal(actual, expected)
 
     def test_estimate(self):
+        self.fail("The test raises an error, fix before starting C implementation.")
         estimator = kde.ModifiedBreimanEstimator(
             dimension=2, sensitivity=0.5, number_of_grid_points=2,
             pilot_kernel=TestKernel(), kernel=TestKernel(),
