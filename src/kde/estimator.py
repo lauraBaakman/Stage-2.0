c1227751d02687ed400bb69d93b8335d35482aa8
Laura Baakman <laurabaakman@gmail.com>
2017-01-30 11:22:14 +0100

    Changed the way validators are used and added a estimate function that should be overloaded.

--- src/kde/estimator.py
+++ src/kde/estimator.py
@@ -1,3 +1,6 @@
+from kde.datavalidation import EstimatorDataValidator
+
+
 class Estimator(object):
 
     def __init__(self, xi_s, x_s, dimension, kernel, general_bandwidth):
@@ -7,6 +10,12 @@ class Estimator(object):
         self._xi_s = xi_s
         self._general_bandwidth = general_bandwidth
         self._x_s = x_s
+        self._data_validator = EstimatorDataValidator(
+            x_s=self._x_s, xi_s=self._xi_s
+        ).validate()
+
+    def estimate(self):
+        raise NotImplementedError()
 
     @property
     def num_x_s(self):
@@ -17,6 +26,3 @@ class Estimator(object):
     def num_xi_s(self):
         (n, _) = self._xi_s.shape
         return n
-
-
-
