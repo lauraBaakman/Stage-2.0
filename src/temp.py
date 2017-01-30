b3f37bce845f398157adb734421a0dede10eb4dc
Laura Baakman <laurabaakman@gmail.com>
2017-01-30 09:36:04 +0100

    Deleted temporary file

--- src/temp.py
+++
@@ -1,8 +0,0 @@
-import kde.kernels._kernels as _kernels
-import numpy as np
-
-if __name__ == '__main__':
-    data = np.array([[1.0, 2.0]])
-
-    density = _kernels.epanechnikov_single_pattern(data)
-    print(density)
