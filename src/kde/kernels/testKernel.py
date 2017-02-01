912aad95b867c9c9888d160461e9d0679436d6b9
Laura Baakman <laurabaakman@gmail.com>
2017-02-01 18:05:23 +0100

    Fixed implementation of the test kernel and its tests.

--- src/kde/kernels/testKernel.py
+++ src/kde/kernels/testKernel.py
@@ -1,7 +1,40 @@
 import numpy as np
 
+from kde.kernels.kernel import Kernel
 
-class TestKernel:
+
+class TestKernel(Kernel):
+    def __init__(self, implementation=None):
+        implementation_class = implementation or _TestKernel_Python
+        self._implementation = implementation_class()
+
+    def to_C_enum(self):
+        return 3
+
+
+class _TestKernel_Python(Kernel):
+    def __init__(self):
+        pass
 
     def evaluate(self, x):
+        if x.ndim == 1:
+            density = self._evaluate_single_pattern(x)
+        elif x.ndim == 2:
+            density = self._evaluate_multiple_patterns(x)
+        else:
+            raise TypeError("Expected a vector or a matrix, not a {}-dimensional array.".format(x.ndim))
+        return density
+
+    def _evaluate_single_pattern(self, x):
+        return np.abs(np.mean(x))
+
+    def _evaluate_multiple_patterns(self, x):
         return np.abs(np.mean(x, axis=1))
+
+
+class _TestKernel_C(Kernel):
+    def __init__(self):
+        pass
+
+    def evaluate(self, x):
+        raise NotImplementedError()
--- src/kde/kernels/tests/test_testKernel.py
+++ src/kde/kernels/tests/test_testKernel.py
@@ -2,18 +2,49 @@ from unittest import TestCase
 
 import numpy as np
 
-from kde.kernels.testKernel import TestKernel
+from kde.kernels.testKernel import TestKernel, _TestKernel_C, _TestKernel_Python
 
 
 class TestTestKernel(TestCase):
-    def test_evaluate_1(self):
+    def test_evaluate_default_implementation(self):
         x = np.array([[2, 3], [3, 4], [4, 5]])
         actual = TestKernel().evaluate(x)
+        expected = np.array([2.5, 3.5, 4.5])
+        np.testing.assert_array_almost_equal(actual, expected)
+
+    def test_evaluate_alternative_implementation(self):
+        x = np.array([[2, 3], [3, 4], [4, 5]])
+        actual = TestKernel(implementation=_TestKernel_C).evaluate(x)
+        expected = np.array([2.5, 3.5, 4.5])
+        np.testing.assert_array_almost_equal(actual, expected)
+
+    def test_to_C_enum(self):
+        expected = 3
+        actual = TestKernel().to_C_enum()
+        self.assertEqual(expected, actual)
+
+
+class TestKernelImpAbstractTest(object):
+    def test_evaluate_1(self):
+        x = np.array([[2, 3], [3, 4], [4, 5]])
+        actual = self._kernel_class().evaluate(x)
         expected = np.array([2.5, 3.5, 4.5])
         np.testing.assert_array_equal(actual, expected)
 
     def test_evaluate_2(self):
         x = np.array([[-2, -3], [-3, -4], [-4, -5]])
-        actual = TestKernel().evaluate(x)
+        actual = self._kernel_class().evaluate(x)
         expected = np.array([2.5, 3.5, 4.5])
         np.testing.assert_array_equal(actual, expected)
+
+
+class TestTestKernel_C(TestKernelImpAbstractTest, TestCase):
+    def setUp(self):
+        super().setUp()
+        self._kernel_class = _TestKernel_C
+
+
+class TestTestKernel_Python(TestKernelImpAbstractTest, TestCase):
+    def setUp(self):
+        super().setUp()
+        self._kernel_class = _TestKernel_Python
