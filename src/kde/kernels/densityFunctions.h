b81fffc5c236e631055dff678c00551bc753872c
Laura Baakman <laurabaakman@gmail.com>
2017-02-08 10:11:00 +0100

    Introduced internal header for densityFunctions.

--- src/kde/kernels/densityFunctions.h
+++ src/kde/kernels/densityFunctions.h
@@ -1,13 +1,5 @@
-//
-// Created by Laura Baakman on 19/12/2016.
-//
-
 #ifndef KERNELS_DENSITYFUNCTIONS_H
 #define KERNELS_DENSITYFUNCTIONS_H
-
-#include <printf.h>
-#include <math.h>
-#include "../utils.h"
 
 typedef double (*KernelDensityFunction)(double* data, int dimensionality, double factor);
 typedef double (*KernelConstantFunction)(int dimensionality);
@@ -16,20 +8,8 @@ typedef struct Kernel {
     KernelDensityFunction densityFunction;
 } Kernel;
 
-double standardGaussianConstant(int patternDimensionality);
-double standardGaussianPDF(double *pattern, int patternDimensionality, double constant);
-
-double epanechnikovConstant(int dimensionality);
-double epanechnikovPDF(double *data, int dimensionality, double constant);
-
-double testKernelConstant(int patternDimensionality);
-double testKernelPDF(double *data, int dimensionality, double constant);
-
 extern Kernel standardGaussianKernel;
 extern Kernel epanechnikovKernel;
 extern Kernel testKernel;
-
-//Auxilaries that should be in a internal header....
-double dotProduct(double *a, double *b, int length);
 
 #endif //KERNELS_DENSITYFUNCTIONS_H
