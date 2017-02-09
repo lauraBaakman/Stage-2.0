d76ab831ebdd2b96e0f34d6869ee3c91776e9123
Laura Baakman <laurabaakman@gmail.com>
2017-02-09 12:35:20 +0100

    Fixed the results validator.

--- src/dataset/results.py
+++ src/dataset/results.py
@@ -24,7 +24,7 @@ class _ResultsWriter(object):
 class _ResultsValidator(object):
     def __init__(self, data_set, results_array):
         self._data_set = data_set
-        self._results = results_array
+        self._results_array = results_array
 
     def validate(self):
         self._one_result_per_pattern()
@@ -32,19 +32,19 @@ class _ResultsValidator(object):
 
     def _one_result_per_pattern(self):
         num_patterns = self._data_set.num_patterns
-        num_results = self._data_set.num_results
+        (num_results,) = self._results_array.shape
         if not num_results == num_patterns:
             raise InvalidResultsException(
                 '''The number of results (should be equal to the number of patterns. The data set has {} patterns, '''
-                '''and {} results.'''
+                '''and there are {} results.'''
                     .format(num_patterns, num_results)
             )
 
     def _results_is_1D_array(self):
-        if self._results.ndim is not 1:
+        if self._results_array.ndim is not 1:
             raise InvalidResultsException(
                 '''1D arrays are expected as results, the current results array has {} dimensions.'''
-                    .format(self._results.ndim)
+                    .format(self._results_array.ndim)
             )
 
 
