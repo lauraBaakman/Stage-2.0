b62c451d9c03c7f578d066f41735af32b96e280d
Laura Baakman <laurabaakman@gmail.com>
2017-02-15 14:43:25 +0100

    Add visualisation code.

---
+++ visualisation/io.R
@@ -0,0 +1,85 @@
+source('header.r')
+
+outputFilePath <- function(inputFilePath, description){
+	inputFile = removeExtension(basename(inputFilePath));
+	outputFile = paste(description, inputFile ,'.pdf', sep='');
+	file.path(imagesOutputPath, outputFile);
+}
+
+parseStringWithInts <- function(string){
+	as.numeric(
+			unlist(
+				regmatches(string, gregexpr("[0-9]+", string))
+			)
+	);
+}
+
+readHeader <- function(filePath){
+	line = readLines(con = filePath, n = 2);
+	list[rows, cols] = parseStringWithInts(line[1]);
+	numberOfPatternsPerSubSet = parseStringWithInts(line[2]);
+	list(
+		rows=rows, 
+		cols=cols, 
+		numberOfPatternsPerSubSet=numberOfPatternsPerSubSet
+	);
+}
+
+readData <- function(filePath, rows){
+	data <- read.csv(
+				file=filePath, 
+				header=FALSE, 
+				dec='.', 
+				row.names = NULL, 
+				skip=2, 
+				nrows=rows, 
+				sep=' ', 
+				col.names = c('x', 'y', 'z')
+			)
+}
+
+readTrueDensities <- function(filePath, rows){
+	densities <- read.csv(
+				file=filePath, 
+				header=FALSE, 
+				dec='.', 
+				row.names = NULL, 
+				skip=2 + rows, 
+				nrows=rows, 
+				sep=' ', 
+				col.names = c('trueDensity')
+			)
+}
+
+readDataSet <- function(filePath){
+	list[rows, cols, numberOfPatternsPerSubSet] <- readHeader(filePath);
+	data <- readData(filePath, rows);
+	densities <- readTrueDensities(filePath, rows);
+	list(
+		data = data, 
+		densities = densities, 
+		numberOfPatternsPerSubSet = numberOfPatternsPerSubSet
+	);
+}
+
+readResults <- function(filePath){
+	read.csv(
+		file=filePath, 
+		header=FALSE, 
+		dec=".", 
+		row.names=NULL, 
+		col.names = c('computedDensity')
+	)	
+}
+
+getDataSetPaths <- function(){
+	list.files(path=dataInputPath, pattern="*.txt", full.names=TRUE, no..=TRUE);
+}
+
+getOutputPaths <- function() {
+	list.files(path=dataOutputPath, pattern="*.txt", full.names=TRUE, no..=TRUE);	
+}
+
+removeExtension <- function(file){
+	 sub("^([^.]*).*", "\\1", file);
+}
---
+++ visualisation/utils.R
@@ -0,0 +1,10 @@
+generateColours <- function(numberOfPatternsPerSubSet){
+	patternColours = list(); 
+	for (idx in 1:length(numberOfPatternsPerSubSet)) {
+		patternColours <- c(patternColours, 
+							rep(colours[idx], numberOfPatternsPerSubSet[idx]),
+							recursive=TRUE
+							);
+	}
+	patternColours;
+}
