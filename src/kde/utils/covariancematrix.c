9dafeaa2ccab00ed41b173c54c60f1c9470f0227
Laura Baakman <laurabaakman@gmail.com>
2017-02-20 14:22:54 +0100

    Set up of the C implementation.

--- src/kde/utils/covariancematrix.c
+++ src/kde/utils/covariancematrix.c
@@ -1,5 +1,26 @@
 #include "covariancematrix.ih"
 
+
 void computeCovarianceMatrix(Array *patterns, Array *covarianceMatrix) {
-    printf("computeCovariance(Array *patterns, Array *covarianceMatrix)");
+//http://stackoverflow.com/a/3307381/1357229
+    double* columnA = patterns->data;
+    double* columnB = NULL;
+    double covariance;
+    for(int row = 0; row < covarianceMatrix->dimensionality; row++, columnA += patterns->colStride){
+        arraySetElement(covarianceMatrix, row, row, computeVariance(columnA, patterns->length));
+        columnB += patterns->stride * (row + 1);
+        for(int col = row  + 1; col < covarianceMatrix->dimensionality; col++, columnB+= patterns->colStride){
+            covariance = computeCovariance(columnA, columnB, patterns->length);
+            arraySetElement(covarianceMatrix, row, col, covariance);
+            arraySetElement(covarianceMatrix, col, row, covariance);
+        }
+    }
+}
+
+double computeCovariance(double *columnA, double *columnB, int length) {
+    return 1.0;
+}
+
+double computeVariance(double *pDouble, int length) {
+    return 42.0;
 }
