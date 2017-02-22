4213af296efc7d61a4d5ce7031ac595cbc10a935
Laura Baakman <laurabaakman@gmail.com>
2017-02-22 12:00:33 +0100

    I am only interested in the eigenvalues, no need for eigenvectors.

--- src/kde/utils/eigenvalues.py
+++ src/kde/utils/eigenvalues.py
@@ -2,16 +2,16 @@ import numpy as np
 import numpy.linalg as LA
 
 
-def _eig_C(data):
+def _eigenvalues_C(data):
     raise NotImplementedError("There is no C implementation of the computation of eigen values.")
 
 
-def _eig_Python(data):
-    eigen_values, eigen_vectors = LA.eig(data)
-    return eigen_values, eigen_vectors
+def _eigenvalues_Python(data):
+    eigen_values, _ = LA.eig(data)
+    return eigen_values
 
 
-def eigenValuesAndVectors(data, implementation=_eig_Python):
+def eigenvalues(data, implementation=_eigenvalues_Python):
     """
     :param data:
     :param implementation:
