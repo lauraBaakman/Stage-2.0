d2aee8989bfa183c0dddb696da780ac6c29ab0d8
Laura Baakman <laurabaakman@gmail.com>
2017-02-22 10:12:52 +0100

    Set up of the eigenvalues methods.

---
+++ src/kde/utils/eigenvaluesvectors.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def _eig_C(data):
+    raise NotImplementedError()
+
+
+def _eig_Python(data):
+    raise NotImplementedError()
+
+
+def _eig(data, implementation=_eig_Python):
+    return implementation(data)
