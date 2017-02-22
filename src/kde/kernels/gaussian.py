58fb914b639fd2384a37d87aee537eabb7d2f43a
Laura Baakman <laurabaakman@gmail.com>
2017-02-22 16:16:35 +0100

    Declared methods for input validation.

--- src/kde/kernels/gaussian.py
+++ src/kde/kernels/gaussian.py
@@ -1,3 +1,6 @@
+import scipy.stats as stats
+from scipy.stats.mstats import gmean
+
 from kde.kernels.kernel import Kernel
 
 
@@ -11,11 +14,32 @@ class Gaussian(object):
 class _Gaussian(Kernel):
 
     def __init__(self, mean, covariance_matrix):
+        self._validate_parameters(mean, covariance_matrix)
         self._mean = mean
         self._covariance_matrix = covariance_matrix
 
+    def _validate_mean_covariance_combination(self, mean, covariance_matrix):
+        raise NotImplementedError()
+
+    def _validate_eigen_values_pdf_combination(self, eigen_values):
+        raise NotImplementedError()
+
+    def _validate_xs_pdf_combination(self, xs):
+        raise NotImplementedError()
+
     def to_C_enum(self):
         return 3
+
+    def _validate_parameters(self, mean, covariance_matrix):
+        self._validate_mean(mean)
+        self._validate_covariance_matrix(covariance_matrix)
+        self._validate_mean_covariance_combination(mean, covariance_matrix)
+
+    def _validate_mean(self, mean):
+        pass
+
+    def _validate_covariance_matrix(self, covariance_matrix):
+        pass
 
 
 class _Gaussian_C(_Gaussian):
@@ -23,18 +47,24 @@ class _Gaussian_C(_Gaussian):
         super(_Gaussian_C, self).__init__(*args, **kwargs)
 
     def evaluate(self, xs):
+        self._validate_xs_pdf_combination(xs)
         raise NotImplementedError()
 
     def scaling_factor(self, general_bandwidth, eigen_values):
+        self._validate_eigen_values_pdf_combination(eigen_values)
+        # TODO Implement validation of eigen_values in combination with dimension of mean
         raise NotImplementedError()
 
 
 class _Gaussian_Python(Kernel):
     def __init__(self, *args, **kwargs):
         super(_Gaussian_Python, self).__init__(*args, **kwargs)
+        self._kernel = stats.multivariate_normal(mean = self._mean, cov= self._covariance_matrix)
 
     def evaluate(self, xs):
-        raise NotImplementedError()
+        self._validate_xs_pdf_combination(xs)
+        return self._kernel.pdf(xs)
 
     def scaling_factor(self, general_bandwidth, eigen_values):
-        raise NotImplementedError()
+        self._validate_eigen_values_pdf_combination(eigen_values)
+        gmean(eigen_values)
