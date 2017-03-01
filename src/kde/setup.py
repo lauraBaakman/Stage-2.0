49a3f0f8761d9b4e3b9158ff31e96c1c414c468a
Laura Baakman <laurabaakman@gmail.com>
2017-03-01 16:49:58 +0100

    Attempt at different way of setting up that should allow the inclusion of gsl.

--- src/kde/setup.py
+++ src/kde/setup.py
@@ -1,15 +1,34 @@
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
+from distutils.core import setup, Extension
 
-    config = Configuration('',
-                           parent_package,
-                           top_path)
-    config.add_extension(
-        '_kde',  # Name of the extension
-        sources=['utils.c', 'kdeModule.c', 'parzen.c', 'modifeidbreiman.c', 'kernels/kernels.c']
-    )
-    return config
+import numpy.distutils.misc_util as npdisutils
+
+
+# def configuration(parent_package='', top_path=None):
+#     config = Configuration('',
+#                            parent_package,
+#                            top_path)
+#     config.add_extension(
+#         '_kde',  # Name of the extension
+#         sources=['utils.c', 'kdeModule.c', 'parzen.c', 'modifeidbreiman.c', 'kernels/kernels.c']
+#     )
+#     return config
 
 if __name__ == "__main__":
-    from numpy.distutils.core import setup
-    setup(configuration=configuration, requires=['numpy'])
+    # from numpy.distutils.core import setup
+    # setup(configuration=configuration, requires=['numpy'])
+
+    # Source: https://github.com/ezbc/class_work/blob/261e1545aabcaabbfe1e1922b5e198d45cfa88c3/classes/machine_learning/project/gausspy_module/gausspy/tvc/code.c/setup.py
+
+    module = Extension('_kde',
+                       library_dirs='/Users/laura/.virtualenvs/stage/lib',
+                       include_dirs=['/Users/laura/.virtualenvs/stage/include/python3.5m',
+                                     '/Users/laura/.virtualenvs/stage/lib/python3.5/site-packages/numpy/core/include'
+                                     '/Users/laura/.virtualenvs/stage/include',
+                                     npdisutils.get_numpy_include_dirs()],
+                       sources=['utils.c',
+                                'kdeModule.c',
+                                'parzen.c',
+                                'modifeidbreiman.c',
+                                'kernels/kernels.c']
+                       )
+    setup(name='_kde', version='1.0', ext_modules=[module])
