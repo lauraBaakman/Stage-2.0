30e67c04e8eba82e4933d6f9a306b6b17467484e
Laura Baakman <laurabaakman@gmail.com>
2017-03-02 15:08:26 +0100

    We can compute the eigenvalues and eigenvectors of matrix.

--- src/kde/utils/eigenvalues.c
+++ src/kde/utils/eigenvalues.c
@@ -1,14 +1,22 @@
 #include <gsl/gsl_eigen.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_vector_double.h>
+
 #include "eigenvalues.h"
 
 void computeEigenValues(Array *data, Array *eigenValues) {
     size_t matrixOrder = (size_t) data->dimensionality;
 
-    gsl_matrix_view gslMatrix = gsl_matrix_view_array (data->data, matrixOrder, matrixOrder);
-    gsl_vector_view gslEigenValues = gsl_vector_view_array(eigenValues->data, matrixOrder);
-    gsl_eigen_symm_workspace * workspace = gsl_eigen_symm_alloc (matrixOrder);
+    gsl_matrix_view matrixView = gsl_matrix_view_array (data->data, matrixOrder, matrixOrder);
+    
+    gsl_vector_view eigenValuesView = gsl_vector_view_array(eigenValues->data, (size_t) eigenValues->length);
+    gsl_matrix *evec = gsl_matrix_alloc (matrixOrder, matrixOrder);
 
-    gsl_eigen_symm(&gslMatrix.matrix, &gslEigenValues.vector, workspace);
+    gsl_eigen_symmv_workspace * w = gsl_eigen_symmv_alloc (matrixOrder);
 
-    gsl_eigen_symm_free (workspace);
+    gsl_eigen_symmv (&matrixView.matrix, &eigenValuesView.vector, evec, w);
+
+    gsl_eigen_symmv_free (w);
+
+    gsl_matrix_free (evec);
 }
