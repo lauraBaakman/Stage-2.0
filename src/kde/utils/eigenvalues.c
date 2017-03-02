fa8528a72e85bf975e19be8fc4b0bfb4a19b3c32
Laura Baakman <laurabaakman@gmail.com>
2017-03-02 13:58:12 +0100

    C implementation of eigenvalue computation.

--- src/kde/utils/eigenvalues.c
+++ src/kde/utils/eigenvalues.c
@@ -1,5 +1,16 @@
+#include <gsl/gsl_eigen.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector_double.h>
 #include "eigenvalues.h"
 
 void computeEigenValues(Array *data, Array *eigenValues) {
-    printf("Hoi!");
+    size_t matrixOrder = (size_t) data->dimensionality;
+
+    gsl_matrix_view gslMatrix = gsl_matrix_view_array (data->data, matrixOrder, matrixOrder);
+    gsl_vector_view gslEigenValues = gsl_vector_view_array(eigenValues->data, matrixOrder);
+    gsl_eigen_symm_workspace * workspace = gsl_eigen_symm_alloc (matrixOrder);
+
+    gsl_eigen_symm(&gslMatrix.matrix, &gslEigenValues.vector, workspace);
+
+    gsl_eigen_symm_free (workspace);
 }
