b8d6a14dfc1d144225d3f71a956f7b73448a6d65
Laura Baakman <laurabaakman@gmail.com>
2017-03-02 11:56:06 +0100

    Changed the way the extensions are build.

--- src/kde/setup.py
+++ src/kde/setup.py
@@ -3,37 +3,24 @@ from distutils.core import setup, Extension
 import numpy.distutils.misc_util as npdisutils
 
 
-# def configuration(parent_package='', top_path=None):
-#     config = Configuration('',
-#                            parent_package,
-#                            top_path)
-#     config.add_extension(
-#         '_kde',  # Name of the extension
-#         sources=['utils.c', 'kdeModule.c', 'parzen.c', 'modifeidbreiman.c', 'kernels/kernels.c']
-#     )
-#     return config
-
 if __name__ == "__main__":
-    # from numpy.distutils.core import setup
-    # setup(configuration=configuration, requires=['numpy'])
-
-    # Source: https://github.com/ezbc/class_work/blob/261e1545aabcaabbfe1e1922b5e198d45cfa88c3/classes/machine_learning/project/gausspy_module/gausspy/tvc/code.c/setup.py
-
     include_dirs =['/Users/laura/.virtualenvs/stage/include/python3.5m',
                    '/Users/laura/.virtualenvs/stage/lib/python3.5/site-packages/numpy/core/include/numpy',
-                   '/Users/laura/.virtualenvs/stage/include']
+                   '/Users/laura/.virtualenvs/stage/include',
+                   '/usr/local/include']
     include_dirs.extend(npdisutils.get_numpy_include_dirs())
-    library_dirs = ['/Users/laura/.virtualenvs/stage/lib']
+    library_dirs = ['/Users/laura/.virtualenvs/stage/lib',
+                    '/usr/local/lib']
+    libraries = ['gsl',
+                 'gslcblas',
+                 'm']
 
-    print(include_dirs)
+    sources = ['utils.c', 'kdeModule.c', 'parzen.c', 'modifeidbreiman.c', 'kernels/kernels.c']
 
     module = Extension('_kde',
                        library_dirs= library_dirs,
+                       libraries=libraries,
                        include_dirs=include_dirs,
-                       sources=['utils.c',
-                                'kdeModule.c',
-                                'parzen.c',
-                                'modifeidbreiman.c',
-                                'kernels/kernels.c']
+                       sources=sources
                        )
     setup(name='_kde', version='1.0', ext_modules=[module])
