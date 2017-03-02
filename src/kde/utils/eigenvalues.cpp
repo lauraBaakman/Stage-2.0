8a9ee40f61857ea23e4db74c82f8cbd56af5dc5f
Laura Baakman <laurabaakman@gmail.com>
2017-03-02 12:54:48 +0100

    Changed extension to c and removed silly comments.

--- src/kde/utils/eigenvalues.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Laura Baakman on 02/03/2017.
-//
-
-#include "eigenvalues.h"
--- src/kde/utils/eigenvalues.h
+++ src/kde/utils/eigenvalues.h
@@ -1,7 +1,3 @@
-//
-// Created by Laura Baakman on 02/03/2017.
-//
-
 #ifndef KERNELS_EIGENVALUES_H
 #define KERNELS_EIGENVALUES_H
 
