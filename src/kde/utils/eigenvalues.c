01707ee8b0422fc17e481bf24ca8f048bd614877
Laura Baakman <laurabaakman@gmail.com>
2017-03-02 15:10:33 +0100

    Stopped computing the eigenvectors.

--- src/kde/utils/eigenvalues.c
+++ src/kde/utils/eigenvalues.c
@@ -8,15 +8,11 @@ void computeEigenValues(Array *data, Array *eigenValues) {
     size_t matrixOrder = (size_t) data->dimensionality;
 
     gsl_matrix_view matrixView = gsl_matrix_view_array (data->data, matrixOrder, matrixOrder);
-    
     gsl_vector_view eigenValuesView = gsl_vector_view_array(eigenValues->data, (size_t) eigenValues->length);
-    gsl_matrix *evec = gsl_matrix_alloc (matrixOrder, matrixOrder);
 
-    gsl_eigen_symmv_workspace * w = gsl_eigen_symmv_alloc (matrixOrder);
+    gsl_eigen_symm_workspace * w = gsl_eigen_symm_alloc(matrixOrder);
 
-    gsl_eigen_symmv (&matrixView.matrix, &eigenValuesView.vector, evec, w);
+    gsl_eigen_symm(&matrixView.matrix, &eigenValuesView.vector, w);
 
-    gsl_eigen_symmv_free (w);
-
-    gsl_matrix_free (evec);
+    gsl_eigen_symm_free (w);
 }
