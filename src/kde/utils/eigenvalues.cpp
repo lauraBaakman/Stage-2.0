1ca138a9f2b344f02de64b97f33f1eede101bbb7
Laura Baakman <laurabaakman@gmail.com>
2017-03-02 12:54:05 +0100

    Files for the eigenvalue computation.

---
+++ src/kde/utils/eigenvalues.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Laura Baakman on 02/03/2017.
+//
+
+#include "eigenvalues.h"
---
+++ src/kde/utils/eigenvalues.h
@@ -0,0 +1,8 @@
+//
+// Created by Laura Baakman on 02/03/2017.
+//
+
+#ifndef KERNELS_EIGENVALUES_H
+#define KERNELS_EIGENVALUES_H
+
+#endif //KERNELS_EIGENVALUES_H
