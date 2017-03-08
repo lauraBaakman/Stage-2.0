a782236b762b702f06d698c1df0cf64979c80d6b
Laura Baakman <laurabaakman@gmail.com>
2017-03-08 16:58:36 +0100

    Add stub for the C implementation of the geometric mean.

---
+++ src/kde/utils/geometricmean.c
@@ -0,0 +1,5 @@
+#include "geometricmean.h"
+
+double computeGeometricMean(double *values, size_t length) {
+    return 42.0;
+}
---
+++ src/kde/utils/geometricmean.h
@@ -0,0 +1,8 @@
+#ifndef KERNELS_GEOMETRICMEAN_H
+#define KERNELS_GEOMETRICMEAN_H
+
+#include <stddef.h>
+
+double computeGeometricMean(double *values, size_t length);
+
+#endif //KERNELS_GEOMETRICMEAN_H
