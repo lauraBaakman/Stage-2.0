56df3e1b743a9761345b3ab034071fdc9338d596
Laura Baakman <laurabaakman@gmail.com>
2017-03-20 16:47:03 +0100

    Support both the C and python implementation of the scaling factor.

--- src/kde/kernels/scaling.py
+++ src/kde/kernels/scaling.py
@@ -1,2 +1,11 @@
-def scaling_factor(general_bandwidth, covariancematrix):
+def _scaling_factor_Python(general_bandwidth, covariance_matrix):
     raise NotImplementedError()
+
+
+def _scaling_factor_C(general_bandwidth, covariance_matrix):
+    raise NotImplementedError()
+
+
+def scaling_factor(general_bandwidth, covariance_matrix, implementation=_scaling_factor_Python):
+    return implementation(general_bandwidth, covariance_matrix)
+
