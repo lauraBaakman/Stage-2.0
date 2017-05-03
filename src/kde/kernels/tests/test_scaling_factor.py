Fixed test to test for the new definition of the scaling factor.

src/kde/kernels/tests/test_scaling_factor.py
@@ -7,20 +7,27 @@ import kde.kernels.scaling as scaling
 
 class TestScaling_factor(TestCase):
     def _scaling_factor_not_needed(self, dimension, implementation):
-        standard_deviation = 0.5
-        variance = standard_deviation * standard_deviation
-        covariance_matrix = variance * np.identity(dimension)
-        actual = implementation(general_bandwidth=standard_deviation,
+        h = 0.5
+        covariance_matrix = h * np.identity(dimension)
+        actual = implementation(general_bandwidth=h,
                                 covariance_matrix=covariance_matrix)
         expected = 1.0
         self.assertAlmostEqual(actual, expected)
 
-    def _scaling_factor_2_auxilary(self, implementation):
+    def _scaling_factor_3_auxiliary(self, dimension, implementation):
+        h = 0.5
+        covariance_matrix = np.identity(dimension)
+        actual = implementation(general_bandwidth=h,
+                                covariance_matrix=covariance_matrix)
+        expected = h
+        self.assertAlmostEqual(actual, expected)
+
+    def _scaling_factor_2_auxiliary(self, implementation):
         general_bandwidth = 0.5
-        covariance_matrix = np.array([[0.085990572233016,   0.006485451830366,   0.012006730455573],
-                                      [0.006485451830366,   0.081172010593423,   0.007497828233425],
-                                      [0.012006730455573,   0.007497828233425,   0.085865865754708]])
-        expected = 5.188704957608463
+        covariance_matrix = np.array([[0.082389650806275, 0.003023884755706, -0.000211227775848],
+                                      [0.003023884755706, 0.083836642887593, -0.002950447026358],
+                                      [-0.000211227775848, -0.002950447026358, 0.086068346308987]])
+        expected = 8.431798865555362e+02
         actual = implementation(general_bandwidth=general_bandwidth,
                                 covariance_matrix=covariance_matrix)
         self.assertAlmostEqual(expected, actual)
@@ -32,7 +39,10 @@ class TestScaling_factor(TestCase):
         self._scaling_factor_not_needed(5, scaling._scaling_factor_python)
 
     def test_scaling_factor_2_python(self):
-        self._scaling_factor_2_auxilary(scaling._scaling_factor_python)
+        self._scaling_factor_2_auxiliary(scaling._scaling_factor_python)
+
+    def test_scaling_factor_3_python(self):
+        self._scaling_factor_3_auxiliary(scaling._scaling_factor_python)
 
     def test_scaling_factor_0_c(self):
         self._scaling_factor_not_needed(2, scaling._scaling_factor_c)
@@ -41,23 +51,26 @@ class TestScaling_factor(TestCase):
         self._scaling_factor_not_needed(5, scaling._scaling_factor_c)
 
     def test_scaling_factor_2_c(self):
-        self._scaling_factor_2_auxilary(scaling._scaling_factor_c)
+        self._scaling_factor_2_auxiliary(scaling._scaling_factor_c)
+
+    def test_scaling_factor_3_c(self):
+        self._scaling_factor_3_auxiliary(scaling._scaling_factor_c)
 
     def test_scaling_factor_c(self):
-        standard_deviation = 0.5
-        variance = standard_deviation * standard_deviation
-        covariance_matrix = variance * np.identity(3)
-        actual = scaling.scaling_factor(general_bandwidth=standard_deviation,
+        h = 0.5
+        dimension = 5
+        covariance_matrix = h * np.identity(dimension)
+        actual = scaling.scaling_factor(general_bandwidth=h,
                                         covariance_matrix=covariance_matrix,
                                         implementation=scaling._scaling_factor_c)
         expected = 1.0
         self.assertAlmostEqual(actual, expected)
 
     def test_scaling_factor_python(self):
-        standard_deviation = 0.5
-        variance = standard_deviation * standard_deviation
-        covariance_matrix = variance * np.identity(3)
-        actual = scaling.scaling_factor(general_bandwidth=standard_deviation,
+        dimension = 5
+        h = 0.5
+        covariance_matrix = h * np.identity(dimension)
+        actual = scaling.scaling_factor(general_bandwidth=h,
                                         covariance_matrix=covariance_matrix,
                                         implementation=scaling._scaling_factor_python)
         expected = 1.0