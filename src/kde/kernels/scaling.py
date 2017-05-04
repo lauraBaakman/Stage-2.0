Fixed the python implementation to use the new formula.

src/kde/kernels/scaling.py
@@ -7,8 +7,8 @@ import kde.kernels._kernels as _kernels
 def _scaling_factor_python(general_bandwidth, covariance_matrix):
     eigen_values = eig.eigenvalues(covariance_matrix)
     (dimension, _) = covariance_matrix.shape
-    bandwidth_term = dimension * np.log(general_bandwidth)
-    eigen_value_term = 0.5 * np.sum(np.log(eigen_values))
+    bandwidth_term = np.log(general_bandwidth)
+    eigen_value_term = (1.0 / dimension) * np.sum(np.log(eigen_values))
     return np.exp(bandwidth_term - eigen_value_term)
 
 