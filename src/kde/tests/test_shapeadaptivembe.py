Fixed other tests that broke due to change in scaling factor.

src/kde/tests/test_shapeadaptivembe.py
@@ -60,6 +60,10 @@ class ShapeAdaptiveMBEImpAbstractTest(object):
         self.fail("Test not yet implemented.")
 
     def test_estimate_gaussian(self):
+        def mock_determine_kernel_shape(pattern):
+            return np.array([[0.60083947, -0.30041974],
+                             [-0.30041974, 0.60083947]])
+
         xi_s = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
         x_s = np.array([[0, 0], [1, 1]])
         local_bandwidths = np.array([0.84089642,
@@ -73,6 +77,7 @@ class ShapeAdaptiveMBEImpAbstractTest(object):
             kernel=kernel,
             local_bandwidths=local_bandwidths, general_bandwidth=general_bandwidth
         )
+        estimator._determine_kernel_shape = mock_determine_kernel_shape
         actual = estimator.estimate()
         expected = np.array([0.28379406489797937, 0.28379406489797937])
         np.testing.assert_array_almost_equal(actual, expected)
@@ -117,12 +122,16 @@ class Test_ShapeAdaptiveMBE_Python(ShapeAdaptiveMBEImpAbstractTest, TestCase):
                                      0.840896194313949])
         h = 0.721347520444482
         kernel = Gaussian
+        kernel_shape = np.array([
+            [0.60083947, -0.30041974],
+            [-0.30041974, 0.60083947]
+        ])
         estimator = self._estimator_class(
             xi_s=xi_s, x_s=x_s, dimension=2,
             kernel=kernel,
             local_bandwidths=local_bandwidths, general_bandwidth=h
         )
-        actual = estimator._estimate_pattern(pattern=pattern)
+        actual = estimator._estimate_pattern(pattern=pattern, kernel_shape=kernel_shape)
         expected = 0.28379406489797937
         self.assertAlmostEqual(actual, expected, 5)
 