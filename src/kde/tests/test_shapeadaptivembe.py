Fixed other tests that broke due to change in scaling factor.

src/kde/tests/test_shapeadaptivembe.py
@@ -93,15 +93,17 @@ class Test_ShapeAdaptiveMBE_Python(ShapeAdaptiveMBEImpAbstractTest, TestCase):
                                      0.840896194313949])
         h = 0.721347520444482
         kernel = Gaussian
+
         estimator = self._estimator_class(
             xi_s=xi_s, x_s=x_s, dimension=2,
             kernel=kernel,
             local_bandwidths=local_bandwidths, general_bandwidth=h
         )
+
         actual = estimator._determine_kernel_shape(pattern)
         expected = np.array([
-            [0.60083947, -0.30041974],
-            [-0.30041974, 0.60083947]
+            [0.832940370215782, -0.416470185107891],
+            [- 0.416470185107891, 0.832940370215782]
         ])
         np.testing.assert_array_almost_equal(actual, expected)
 