dd1481e766b913f3f52cbd9b2420a96d2f774940
Laura Baakman <laurabaakman@gmail.com>
2017-05-08 16:57:17 +0200

    Ferdosi 2

--- src/datasets/simulated/ferdosi2.py
+++ src/datasets/simulated/ferdosi2.py
@@ -12,29 +12,15 @@ class Ferdosi3(SimulatedDataSet):
     def _init_components(self):
         self._components['trivariate gaussian 1'] = {
                 'component': components.TrivariateGaussian(
-                    mean=np.array([24, 10, 10]),
-                    covariance_matrix=np.diag(np.array([2, 2, 2]))
+                    mean=np.array([25, 25, 25]),
+                    covariance_matrix=np.diag(np.array([5, 5, 5]))
                 ),
                 'num elements': 20000,
             }
         self._components['trivariate gaussian 2'] = {
                 'component': components.TrivariateGaussian(
-                    mean=np.array([33, 70, 40]),
-                    covariance_matrix=np.diag(np.array([10, 10, 10]))
-                ),
-                'num elements': 20000,
-        }
-        self._components['trivariate gaussian 3'] = {
-                'component': components.TrivariateGaussian(
-                    mean=np.array([90, 20, 80]),
-                    covariance_matrix=np.diag(np.array([1, 1, 1]))
-                ),
-                'num elements': 20000,
-        }
-        self._components['trivariate gaussian 4'] = {
-                'component': components.TrivariateGaussian(
-                    mean=np.array([60, 80, 23]),
-                    covariance_matrix=np.diag(np.array([5, 5, 5]))
+                    mean=np.array([65, 65, 65]),
+                    covariance_matrix=np.diag(np.array([20, 20, 20]))
                 ),
                 'num elements': 20000,
         }
@@ -43,5 +29,5 @@ class Ferdosi3(SimulatedDataSet):
                     minimum_value=0,
                     maximum_value=100
                 ),
-                'num elements': 40000,
+                'num elements': 20000,
         }
