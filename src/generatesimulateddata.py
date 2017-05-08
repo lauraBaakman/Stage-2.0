fda3332cd4a7b964ed0bf722507cd937251f07d8
Laura Baakman <laurabaakman@gmail.com>
2017-05-08 11:20:19 +0200

    Fixed the main that generates all simulated data.

--- src/generatesimulateddata.py
+++ src/generatesimulateddata.py
@@ -1,4 +1,12 @@
+import os.path
+
 import datasets.simulated
 
 if __name__ == '__main__':
-    (patterns, densities) = datasets.simulated.ferdosi_1(6)
+    extension = '.txt'
+    path = '../data/simulated'
+
+    for name, generator in datasets.simulated.generators.items():
+        data_set = generator()
+        path = os.path.join(path, name + extension)
+        data_set.to_file(path)
