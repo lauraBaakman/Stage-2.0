e2edd79282c4b1b923f6282c3267bfe575b07c4c
Laura Baakman <laurabaakman@gmail.com>
2017-05-09 14:40:25 +0200

    Implementation of wall like structure.

--- src/datasets/simulated/components/walllikestructure.py
+++ src/datasets/simulated/components/walllikestructure.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from datasets.simulated.components.component import Component
 
 
@@ -8,7 +10,28 @@ class WallLikeStructure(Component):
         self._one_dimensional_components = one_dimensional_components
 
     def patterns(self, num_patterns):
-        raise NotImplementedError()
+        patterns = list(
+            map(
+                lambda component: component.patterns(num_patterns), self._one_dimensional_components
+            )
+        )
+        return np.hstack(patterns)
 
     def densities(self, patterns):
-        raise NotImplementedError()
+        columns = [
+            np.array([np.array(element, ndmin=1)
+                      for element
+                      in column])
+            for column
+            in
+            patterns.transpose()
+        ]
+        column_component_pairs = zip(columns, self._one_dimensional_components)
+
+        one_dimensional_densities = list()
+        for (column, component) in column_component_pairs:
+            column_densities = component.densities(column)
+            one_dimensional_densities.append(column_densities)
+
+        densities = np.prod(one_dimensional_densities, axis=0)
+        return densities
