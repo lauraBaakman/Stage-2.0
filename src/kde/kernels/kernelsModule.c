48b7463da3c72f5b868a7b752987b2098dc4f0cd
Laura Baakman <laurabaakman@gmail.com>
2017-05-11 14:11:02 +0200

    Remove unused kernelConstant.

--- src/kde/kernels/kernelsModule.c
+++ src/kde/kernels/kernelsModule.c
@@ -141,11 +141,9 @@ static PyObject * sa_gaussian_single_pattern(PyObject *self, PyObject *args){
     /* Do computations */
     ShapeAdaptiveKernel kernel = selectShapeAdaptiveKernel(SHAPE_ADAPTIVE_GAUSSIAN);
     gsl_vector_view pattern_view = arrayGetGSLVectorView(&pattern);
-    gsl_matrix* kernelConstant = kernel.factorFunction(&globalBandwidthMatrix);
     double density = kernel.densityFunction(&pattern_view.vector, localBandwidth, &globalBandwidthMatrixView.matrix);
 
     /* Free memory */
-    gsl_matrix_free(kernelConstant);
 
     /* Create return object */
     PyObject *returnObject = Py_BuildValue("d", density);
