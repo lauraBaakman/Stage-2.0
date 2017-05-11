6d3b45dca52c2fc45d6e3812a7ad7208fdb7af34
Laura Baakman <laurabaakman@gmail.com>
2017-05-11 08:35:33 +0200

    Changed the benchmark script.

--- src/benchmark.py
+++ src/benchmark.py
@@ -2,30 +2,38 @@ import timeit
 
 import numpy as np
 
+import kde.kernels.shapeadaptivegaussian as sa_gaussian
+
 
 def test_1():
-    dimension = 5
-    window_width = 0.25
+    dimension = 3
+    local_bandwidth = np.random.rand()
+    pattern = np.random.rand(1, dimension)
 
-    patterns = np.random.rand(5000, dimension)
-    data_points = np.random.rand(500, dimension)
+    H = np.array([[2, -1, 0],
+                  [-1, 2, -1],
+                  [0, -1, 2]])
 
-def test_2():
-    dimension = 5
-    window_width = 0.25
+    kernel = sa_gaussian.ShapeAdaptiveGaussian(H)
+    kernel.evaluate(pattern, local_bandwidth)
 
-    patterns = np.random.rand(5000, dimension)
-    data_points = np.random.rand(500, dimension)
+
+# def test_2():
+#     dimension = 5
+#     window_width = 0.25
+#
+#     patterns = np.random.rand(5000, dimension)
+#     data_points = np.random.rand(500, dimension)
 
 
 if __name__ == '__main__':
     print(timeit.timeit(
         "test_1()",
         setup="from __main__ import test_1",
-        number=5000)
+        number=10000)
     )
-    print(timeit.timeit(
-        "test_2()",
-        setup="from __main__ import test_2",
-        number=5000)
-    )
+    # print(timeit.timeit(
+    #     "test_2()",
+    #     setup="from __main__ import test_2",
+    #     number=5000)
+    # )
