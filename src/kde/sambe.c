3f946a17458980274ef5fc94c38c28629aa9219b
Laura Baakman <laurabaakman@gmail.com>
2017-05-18 13:26:35 +0200

    First steps towards the sambeFinalDensity Implementation.

--- src/kde/sambe.c
+++ src/kde/sambe.c
@@ -1,7 +1,25 @@
 #include "sambe.ih"
 
-double sambeFinalDensity(double *pattern, Array *datapoints, double globalBandwidth) {
-    return 42.0;
+double sambeFinalDensity(double *pattern, Array *datapoints,
+                         double globalBandwidth,
+                         ShapeAdaptiveKernel kernel) {
+
+    size_t dimension = (size_t) datapoints->dimensionality;
+
+    double* currentDataPoint = datapoints->data;
+
+    gsl_matrix* globalKernelShape = determineKernelShape(pattern);
+
+    /* Prepare the evaluation of the kernel */
+    double pdfConstant;
+    double globalScalingFactor;
+    gsl_matrix* globalInverse = gsl_matrix_alloc(dimension, dimension);
+    kernel.factorFunction(globalKernelShape, globalInverse, &globalScalingFactor, &pdfConstant);
+
+
+
+    /* Free memory */
+    gsl_matrix_free(globalInverse);
 }
 
 gsl_matrix *determineKernelShape(double *pattern) {
