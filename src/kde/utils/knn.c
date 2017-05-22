939b3b65de44d98eb99d58495521a77f4c67cae5
Laura Baakman <laurabaakman@gmail.com>
2017-05-22 11:30:36 +0200

    Signature changes.

--- src/kde/utils/knn.c
+++ src/kde/utils/knn.c
@@ -1,4 +1,5 @@
 #include <gsl/gsl_vector_double.h>
+#include <gsl/gsl_matrix.h>
 #include "knn.ih"
 
 //Array* compute_k_nearest_neighbours(int k, int patternIdx, Array *patterns, Array *distanceMatrix,
@@ -20,7 +21,11 @@ void compute_k_nearest_neighbours(int k, int patternIdx, gsl_matrix *patterns, g
                                   gsl_matrix *outNearestNeighbours) {
     gsl_vector_view distances = gsl_matrix_row(distanceMatrix, patternIdx);
 
+    size_t distanceCount = distanceMatrix->size1;
+
     ListElement* elements = toArrayOfListElements(&distances.vector);
+    listElementArraySort(elements, distanceCount);
+
 
     free(elements);
 }
@@ -34,8 +39,8 @@ ListElement* toArrayOfListElements(gsl_vector *distances){
     return elements;
 }
 
-void listElementArraySort(ListElement* elements, int numElements){
-    qsort((void *) elements, (size_t) numElements, sizeof(ListElement), listElementCompare);
+void listElementArraySort(ListElement* elements, size_t numElements){
+    qsort((void *) elements, numElements, sizeof(ListElement), listElementCompare);
 }
 
 int listElementCompare(const void *s1, const void *s2){
@@ -47,8 +52,8 @@ int listElementCompare(const void *s1, const void *s2){
     if (*(e1->value) > *(e2->value)) return +1;
 }
 
-void listElementArrayPrint(ListElement* elements, int numElements){
-    for (int i = 0; i < numElements; ++i) {
+void listElementArrayPrint(ListElement* elements, size_t numElements){
+    for (size_t i = 0; i < numElements; ++i) {
         listElementPrint(&elements[i]);
     }
 }
