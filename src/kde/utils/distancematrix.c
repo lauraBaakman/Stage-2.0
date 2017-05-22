518c79574a225060103ee52a694d6ac7aafb7abc
Laura Baakman <laurabaakman@gmail.com>
2017-05-22 11:08:02 +0200

    Remove dead code.

--- src/kde/utils/distancematrix.c
+++ src/kde/utils/distancematrix.c
@@ -1,50 +1,35 @@
 #include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector_double.h>
 #include "distancematrix.ih"
 
-//void computeDistanceMatrix(Array *patterns, Array *distanceMatrix){
-//
-//    double* a = patterns->data;
-//    double* b;
-//    double distance;
-//
-//    arraySetDiagonalToZero(distanceMatrix);
-//
-//    for(int i = 0;
-//            i < patterns->length;
-//            i++, a+= patterns->rowStride)
-//    {
-//        b = a + patterns->rowStride;
-//        for(int j = i + 1;
-//                j < patterns->length;
-//                j++, b+= patterns->rowStride){
-//            distance = squaredEuclidean(a, b, patterns->dimensionality);
-//            arraySetElement(distanceMatrix, i, j, distance);
-//            arraySetElement(distanceMatrix, j, i, distance);
-//        }
-//    }
-//}
-
-double squaredEuclidean(double* a, double* b, int length){
-    double distance = 0;
-    for(int i = 0; i < length; i++){
-        distance += (a[i] - b[i]) * (a[i] - b[i]);
-    }
-    return distance;
-}
-
 void computeDistanceMatrix(gsl_matrix *patterns, gsl_matrix *distanceMatrix) {
-    gsl_matrix_set_identity(distanceMatrix);
+    gsl_matrix_set_zero(distanceMatrix);
 
-    size_t num_patterns = patterns->size1;
-    size_t dimension = patterns->size2;
+    gsl_vector_view a, b;
+
+    size_t patternCount = patterns->size1;
 
     double distance;
-    for(int i = 0; i < distanceMatrix->size1; i++){
-        for(int j = i + 1; j < distanceMatrix->size1; j++){
-//            distance = squaredEuclidean(a, b, dimension);
-            distance = 42.0;
+    for(size_t i = 0; i < patternCount; i++){
+        a = gsl_matrix_row(patterns, i);
+        for(size_t j = i + 1; j < patternCount; j++){
+            b = gsl_matrix_row(patterns, j);
+
+            distance = squaredEuclidean(&a.vector, &b.vector);
+
             gsl_matrix_set(distanceMatrix, i, j, distance);
             gsl_matrix_set(distanceMatrix, j, i, distance);
         }
     }
+}
+
+double squaredEuclidean(gsl_vector* a, gsl_vector* b){
+    size_t vectorDimension = a->size;
+    double distance = 0;
+    double difference;
+    for(size_t i = 0; i < vectorDimension; i++){
+        difference = a->data[i] - b->data[i];
+        distance += difference * difference;
+    }
+    return distance;
 }
