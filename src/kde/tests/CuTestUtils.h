5bda414f5801ae4dbce5fc75227decd1c0e2497e
Laura Baakman <laurabaakman@gmail.com>
2017-05-23 15:32:45 +0200

    Add an isnan.

--- src/kde/tests/CuTestUtils.h
+++ src/kde/tests/CuTestUtils.h
@@ -3,6 +3,13 @@
 
 #include "CuTest.h"
 #include <gsl/gsl_matrix.h>
+#include <stdbool.h>
+
+#ifndef isnan
+inline bool isnan(double x) {
+    return x != x;
+}
+#endif
 
 void CuAssertMatrixEquals_LineMsg(CuTest* tc,
                                const char* file, int line, const char* message,
