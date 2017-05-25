a387c9b859a038937eb81b68b0c73f842aee1ce7
Laura Baakman <laurabaakman@gmail.com>
2017-05-25 14:52:33 +0200

    Removed dead code.

--- src/kde/kernels/epanechnikov.c
+++ src/kde/kernels/epanechnikov.c
@@ -32,11 +32,3 @@ double epanechnikovConstant(int dimensionality) {
     double denominator = gamma(dimensionality / 2.0 + 1);
     return 2 * (numerator / denominator);
 }
-
-double epanechnikovPDF(gsl_vector* pattern, double constant) {
-    double dotProduct = 0.0;
-    gsl_blas_ddot(pattern,  pattern, &dotProduct);
-    if (dotProduct >= 1) return 0;
-    double numerator = (double) pattern->size + 2;
-    return (numerator / constant) * (1 - pattern->size);
-}
