543daf3340c65eb6cb15f780566e0060248fa49b
Laura Baakman <laurabaakman@gmail.com>
2017-05-31 15:32:32 +0200

    Added ferdosi 4 and 5.

--- src/datasets/simulated/allsets.py
+++ src/datasets/simulated/allsets.py
@@ -1,9 +1,13 @@
 from datasets.simulated.ferdosi1 import Ferdosi1
 from datasets.simulated.ferdosi2 import Ferdosi2
 from datasets.simulated.ferdosi3 import Ferdosi3
+from datasets.simulated.ferdosi4 import Ferdosi4
+from datasets.simulated.ferdosi5 import Ferdosi5
 
 sets = {
     'ferdosi 1': Ferdosi1,
     'ferdosi 2': Ferdosi2,
     'ferdosi 3': Ferdosi3,
+    'ferdosi 4': Ferdosi4,
+    'ferdosi 5': Ferdosi5,
 }
