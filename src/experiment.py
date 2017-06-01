2d595ee314e58e94d4fda7aec58e1445bcd73e91
Laura Baakman <laurabaakman@gmail.com>
2017-06-01 16:18:10 +0200

    Pass the results path as a parameter.

--- src/experiment.py
+++ src/experiment.py
@@ -9,7 +9,7 @@ import inputoutput
 
 _data_set_path = Path(
     '/Users/laura/Repositories/stage-2.0/data/simulated/small/')
-_result_path = Path(
+_results_path = Path(
     '/Users/laura/Repositories/stage-2.0/results/simulated/small/')
 
 sensitivities = {
@@ -102,7 +102,7 @@ def build_output_path(data_set_file, estimator, sensitivity):
         estimator=estimator,
         sensitivity=sensitivity
     )
-    return _result_path.child(out_file_name)
+    return _results_path.child(out_file_name)
 
 
 class InputDirectoryAction(argparse.Action):
@@ -120,21 +120,38 @@ class InputDirectoryAction(argparse.Action):
             )
 
 
+class OutputDirectoryAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        prospective_dir = Path(values).absolute()
+        prospective_dir.mkdir(parents=True)
+        if not os.path.isdir(prospective_dir):
+            raise argparse.ArgumentTypeError(
+                "{0} is not a valid path".format(prospective_dir)
+            )
+        if os.access(prospective_dir, os.R_OK):
+            setattr(namespace, self.dest, prospective_dir)
+        else:
+            raise argparse.ArgumentTypeError(
+                "{0} is not a readable dir".format(prospective_dir)
+            )
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input_directory',
                         action=InputDirectoryAction,
                         default=_data_set_path)
+    parser.add_argument('-o', '--output_directory',
+                        action=OutputDirectoryAction,
+                        default=_results_path)
     args = parser.parse_args()
 
-    _result_path.mkdir(parents=True)
     data_set_path = args.input_directory
+    _results_path = args.output_directory
 
+    data_set_files = get_data_set_files(data_set_path)
 
     for data_set_file in data_set_files:
         with open(data_set_file, 'r') as in_file:
             print("Data set: {}".format(data_set_file))
             data_set = inputoutput.DataSet.from_file(in_file=data_set_file)
             handle_dataset(data_set)
-    data_set_files = get_data_set_files(data_set_path)
-
