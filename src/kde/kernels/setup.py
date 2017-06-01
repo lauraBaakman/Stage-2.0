dbc4215f236509532302e4aaf36025067e8074f1
Laura Baakman <laurabaakman@gmail.com>
2017-06-01 16:31:05 +0200

    Renamed file.

--- src/kde/kernels/setup.py
+++ src/kde/kernels/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-import globalConfigParameters
+import setup_globals
 
 sources = ['kernelsModule.c',
            'kernels.c',
@@ -14,9 +14,9 @@ sources = ['kernelsModule.c',
 
 if __name__ == "__main__":
     module = Extension('_kernels',
-                       library_dirs=globalConfigParameters.library_dirs,
-                       libraries=globalConfigParameters.libraries,
-                       include_dirs=globalConfigParameters.include_dirs,
+                       library_dirs=setup_globals.library_dirs,
+                       libraries=setup_globals.libraries,
+                       include_dirs=setup_globals.include_dirs,
                        sources=sources
                        )
     setup(name='_kernels', version='1.0', ext_modules=[module])
