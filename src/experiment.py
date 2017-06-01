355eda5eba5d346dc9e961c2f4e8fe2ddeb90bc9
Laura Baakman <laurabaakman@gmail.com>
2017-06-01 14:21:52 +0200

    Finalized the script.

--- src/experiment.py
+++ src/experiment.py
@@ -4,10 +4,10 @@ from unipath import Path
 
 from kde.sambe import SAMBEstimator
 from kde.modifeidbreiman import MBEstimator
-from inputoutput.dataset import DataSet
-from inputoutput.results import Results
+import inputoutput
 
 _data_set_path = Path('/Users/laura/Repositories/stage-2.0/data/simulated/')
+_result_path = Path('/Users/laura/Repositories/stage-2.0/results/simulated/')
 
 sensitivities = {
     'silverman': lambda d: 0.5,
@@ -19,46 +19,67 @@ estimators = {
     'mbe': MBEstimator
 }
 
-data_set_files = [
-    'ferdosi_1_600.txt',
-    'ferdosi_1_60000.txt',
-    'ferdosi_2_60000.txt',
-    'ferdosi_3_120000.txt',
-    'ferdosi_4_60000.txt',
-    'ferdosi_5_60000.txt',
-]
 
+def get_data_set_files(input_path):
 
-def build_input_path(data_set_file):
-    return _data_set_path.child(data_set_file)
+    def add_data_set(data_set):
+        files.append(data_set)
+
+    def skip_data_set(*args, **kwargs):
+        pass
+
+    potential_data_sets = input_path.listdir("*.txt")
+    files = list()
+
+    responses = {
+        'y': add_data_set,
+        'n': skip_data_set
+    }
+
+    for data_set in potential_data_sets:
+        response = input('Include the data set in the file {}? [y/N]\n'.format(data_set)).lower()
+        responses.get(response, skip_data_set)(data_set)
+    return files
 
 
 def handle_dataset(data_set):
     for estimator_name, Estimator in estimators.items():
+        print("\tEstimator: {}".format(estimator_name))
+
         for sensitivity_name, sensitivity in sensitivities.items():
+            print("\t\tSensitivity: {}".format(sensitivity_name))
+
             result = run(data_set,
                          Estimator(
                              dimension=data_set.dimension,
                              sensitivity=sensitivity))
 
             out_path = build_output_path(data_set_file, estimator_name, sensitivity_name)
-            with open(out_path, 'w') as out_file:
+            with open(out_path, 'a') as out_file:
                 result.to_file(out_file)
 
 
 def run(data_set, estimator):
-    densities = np.zeros(data_set.num_patterns)
-    # densities = estimator.estimate(data_set.patterns, data_set.patterns)
-    return Results(densities, data_set)
+    densities = estimator.estimate(data_set.patterns, data_set.patterns)
+    return inputoutput.Results(densities, data_set)
 
 
-def build_output_path(dataset_file, estimator, sensitivity):
-    raise NotImplementedError()
+def build_output_path(data_set_file, estimator, sensitivity):
+    out_file_name = '{data_set}_{estimator}_{sensitivity}.txt'.format(
+        data_set=Path(data_set_file).stem,
+        estimator=estimator,
+        sensitivity=sensitivity
+    )
+    return _result_path.child(out_file_name)
 
 
 if __name__ == '__main__':
+    _result_path.mkdir(parents=True)
+
+    data_set_files = get_data_set_files(_data_set_path)
+
     for data_set_file in data_set_files:
-        in_path = build_input_path(data_set_file)
-        with open(in_path, 'r') as in_file:
-            data_set = DataSet.from_file(in_file=in_path)
+        with open(data_set_file, 'r') as in_file:
+            print("Data set: {}".format(data_set_file))
+            data_set = inputoutput.DataSet.from_file(in_file=data_set_file)
             handle_dataset(data_set)
