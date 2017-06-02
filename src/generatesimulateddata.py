82031ee1ef91d17f10ca81eb859163c2e4bf5dea
Laura Baakman <laurabaakman@gmail.com>
2017-06-02 11:28:11 +0200

    Refactor

--- src/generatesimulateddata.py
+++ src/generatesimulateddata.py
@@ -2,13 +2,19 @@ import os.path
 
 import datasets.simulated.allsets
 
+
+def generate_data_set(name, generator):
+    data_set = generator()
+    out_path = os.path.join(path, name.replace(" ", "_") + "_" + str(data_set.num_patterns) + extension)
+    with open(out_path, 'wb') as out_file:
+        data_set.to_file(out_file)
+
 if __name__ == '__main__':
     extension = '.txt'
     path = '/Users/laura/Repositories/stage-2.0/data/simulated'
 
-    for name, generator in datasets.simulated.allsets.sets.items():
+    data_sets = datasets.simulated.allsets.sets
+
+    for name, generator in data_sets.items():
         print('Generating {} ...'.format(name))
-        data_set = generator()
-        out_path = os.path.join(path, name.replace(" ", "_") + "_" + str(data_set.num_patterns) + extension)
-        with open(out_path, 'wb') as out_file:
-            data_set.to_file(out_file)
+        generate_data_set(name, generator)
