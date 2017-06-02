c781b4ac0b085cb4f13fafd92f0959a16b6bb65b
Laura Baakman <laurabaakman@gmail.com>
2017-06-02 12:10:36 +0200

    Added to the lsit of datasets.

--- src/datasets/simulated/allsets.py
+++ src/datasets/simulated/allsets.py
@@ -3,6 +3,7 @@ from datasets.simulated.ferdosi2 import Ferdosi2
 from datasets.simulated.ferdosi3 import Ferdosi3
 from datasets.simulated.ferdosi4 import Ferdosi4
 from datasets.simulated.ferdosi5 import Ferdosi5
+from datasets.simulated.baakman1 import Baakman1
 
 sets = {
     'ferdosi 1': Ferdosi1,
@@ -10,4 +11,5 @@ sets = {
     'ferdosi 3': Ferdosi3,
     'ferdosi 4': Ferdosi4,
     'ferdosi 5': Ferdosi5,
+    'baakman 1': Baakman1
 }
