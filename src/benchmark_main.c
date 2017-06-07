e4bdbe6f25e22b076d6ab71c21520e0509ccf49f
Laura Baakman <laurabaakman@gmail.com>
2017-06-07 10:01:15 +0200

    Read parameters from the command line.

--- src/benchmark_main.c
+++ src/benchmark_main.c
@@ -1,13 +1,30 @@
 #include <gsl/gsl_matrix.h>
 
-size_t numXs = 60;
-size_t d = 3;
+size_t numXs;
+size_t numXis;
+size_t dimension;
 
 
-int main(){
-	printf("Hoi!\n");
+void readInput(int argc, char* argv[]){
+	if(argc != 4){
+		fprintf(stderr, "Expected usage:\n\t./benchmark.out numXs, numXis dimension\n");
+		exit(-1);		
+	}
+	if (
+		1 != sscanf(argv[1], "%zu", &numXs) ||
+		1 != sscanf(argv[2], "%zu", &numXis) ||
+		1 != sscanf(argv[3], "%zu", &dimension)
+	){
+		fprintf(stderr, "Sscanf failed.\n");
+		exit(-1);
+	}
+	printf("numXs: %zu, numXis: %zu, dimension: %zu\n", numXs, numXis, dimension);	
+}
 
-    gsl_matrix* xs = gsl_matrix_alloc(numXs, d);
+int main(int argc, char* argv[]){
+	readInput(argc, argv);
+
+    gsl_matrix* xs = gsl_matrix_alloc(numXs, dimension);
     gsl_matrix_set_all(xs, 0.5);
 
     gsl_matrix_free(xs);
