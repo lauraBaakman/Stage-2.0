71c0c528025f43a124e0335e439fc8e0e85d256d
Laura Baakman <laurabaakman@gmail.com>
2017-06-07 10:19:44 +0200

    Benchmark parzen.

--- src/benchmark_main.c
+++ src/benchmark_main.c
@@ -1,4 +1,9 @@
+#include <time.h>
+
 #include <gsl/gsl_matrix.h>
+
+#include "kde/parzen.h"
+#include "kde/kernels/kernels.h"
 
 size_t numXs;
 size_t numXis;
@@ -21,12 +26,40 @@ void readInput(int argc, char* argv[]){
 	printf("numXs: %zu, numXis: %zu, dimension: %zu\n", numXs, numXis, dimension);	
 }
 
-int main(int argc, char* argv[]){
-	readInput(argc, argv);
-
+clock_t benchmarkParzen(){
     gsl_matrix* xs = gsl_matrix_alloc(numXs, dimension);
     gsl_matrix_set_all(xs, 0.5);
 
+    gsl_matrix* xis = gsl_matrix_alloc(numXis, dimension);
+    gsl_matrix_set_all(xis, 0.5);
+
+    gsl_vector* densities = gsl_vector_alloc(numXs);
+
+    double windowWidth = 4;
+
+    SymmetricKernel kernel = selectSymmetricKernel(EPANECHNIKOV);
+
+    clock_t tic = clock();
+    parzen(xs, xis, windowWidth, kernel, densities);
+    clock_t toc = clock();
+
+    gsl_vector_free(densities);
+    gsl_matrix_free(xis);
     gsl_matrix_free(xs);
+
+    return toc - tic;	
+}
+
+void printTimeInformation(clock_t tictoc, char* estimator){
+	 printf("%s: %f seconds\n", estimator, (double)(tictoc) / CLOCKS_PER_SEC);
+}
+
+int main(int argc, char* argv[]){
+	readInput(argc, argv);
+
+	printTimeInformation(
+		benchmarkParzen(), "parzen"
+	);
+
     return 0;
 }
