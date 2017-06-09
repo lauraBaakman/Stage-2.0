bee0b4193362fd73bc40afc8343dca68a692653a
Laura Baakman <laurabaakman@gmail.com>
2017-06-09 12:54:15 +0200

    use 3d patterns.

--- src/kde/utils/knn.c
+++ src/kde/utils/knn.c
@@ -23,28 +23,28 @@ void computeKNearestNeighbours(size_t k, size_t patternIdx, gsl_matrix *patterns
 }
 
 void computeNearestNeighboursKD(gsl_vector* pattern, int k){
-    void* tree = kd_create(2);
+    void* tree = kd_create(3);
     void* res;
     double data[] = {
-            2, 3,
-            5, 4,
-            9, 6,
-            4, 7,
-            8, 1,
-            7, 2
+            2, 3, 0,
+            5, 4, 0,
+            9, 6, 0,
+            4, 7, 0,
+            8, 1, 0,
+            7, 2, 0,
     };
-    double pos[2] = {9, 2};
+    double pos[3] = {9, 2, 0};
     kd_insert(tree, &data[0], NULL);
-    kd_insert(tree, &data[2], NULL);
-    kd_insert(tree, &data[4], NULL);
+    kd_insert(tree, &data[3], NULL);
     kd_insert(tree, &data[6], NULL);
-    kd_insert(tree, &data[8], NULL);
-    kd_insert(tree, &data[10], NULL);
+    kd_insert(tree, &data[9], NULL);
+    kd_insert(tree, &data[12], NULL);
+    kd_insert(tree, &data[15], NULL);
 
     res = kd_nearest_n(tree, pos, 3);
     while(!kd_res_end(res)) {
         kd_res_item(res, &data[0]);
-        printf("%f %f %f\n", data[0], data[1], kd_res_dist(res));
+        printf("%1.0f %1.0f %1.0f distance: %2.3f\n", data[0], data[1], data[2],  kd_res_dist(res));
         kd_res_next(res);
     }
     kd_res_free(res);
