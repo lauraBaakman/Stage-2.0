e7b461c8b6d1a6fac6201d7b219e913e0c97b2b3
Laura Baakman <laurabaakman@gmail.com>
2017-07-11 21:47:10 +0200

    Do the difference visualization without plotly.

--- visualisation/differencePlot.R
+++ visualisation/differencePlot.R
@@ -1,49 +1,95 @@
+# Remove al existing variables
+rm(list = ls())
+
+# Load external methods and variables
+source("./header.R");
+source("./io.R");
+
 # Load libraries
-library(plotly)
+library(scatterplot3d)
+# https://github.com/bhaskarvk/colormap
+library(colormap)
 
 createDifferencePlot<-function(data, values_1, values_2, plot_title){
-  data$differences = computeSquaredError(values_1$computedDensity, values_2$computedDensity);
-  data <- arrange(data, desc(differences));
+  data$differences = computeSquaredError(values_1, values_2);
+  data$normalizedDifferences = scaleToUnitRange(data$differences);
   
-  plot = plot_ly(
-    data,     
-    x=~x, y=~y, z=~z,
-    type = "scatter3d", mode="markers",
-    size = ~differences,
-    marker = list(
-      color=~differences, 
-      colorscale = "Blackbody",
-      reversescale=TRUE,
-      showscale = TRUE,
-      symbol='circle',
-      sizemode='diameter',
-      colorbar = list(
-        title='squared error',
-        thickness=15,
-        titleside='bottom'
-      )
-    ),
-    sizes= c(8, 30)
-  ) %>%
-  layout(
-    title = plot_title
-  ) 
-  plot;
+  sizes = computeSizes(data$normalizedDifferences, maxSize = 5, minSize = 0.5);
+  colormap = computeColorMap(numShades = 15);
+  colors = computeColors(data$normalizedDifferences, colormap);
+  
+  #           b,   l, t, r)
+  margins = c(2.4, 3, 1, 5)
+  glyph=16
+  
+  plot <- scatterplot3d(
+    x = data$x, y = data$y, z = data$z,
+    xlab='x', ylab='y', zlab='z',
+    pch=glyph,
+    color=colors,
+    grid=FALSE,
+    lty.hide=4,
+    cex.lab=1.0,
+    label.tick.marks=TRUE,
+    mar=margins,
+    cex.symbols = sizes,
+    main=plot_title
+  )
+  par(mar = margins)
+  par(xpd=TRUE)
+  legend(plot$xyz.convert(105, 100, 100), 
+         pch=glyph,
+         col= colormap,
+         cex=0.9,
+         horiz=FALSE,
+         # lty=c(1,1),
+         legend = computeLegendValues(data$differences, colormap)
+  )
+  return(plot);
 }
 
 computeSquaredError<-function(values_1, values_2){
   (values_1 - values_2)^2
 }
 
-publishDifferencePlotLocally<-function(plot, file_name, outfilepath){
-  api_create(plot, filename=file_name, sharing='public')
-  plotly_IMAGE(plot, format = "png", out_file = outfilepath)
+computeSizes<-function(values, minSize=2.0, maxSize=20){
+  sizes = minSize + values * (maxSize - minSize);
+  return(sizes);
 }
 
-publishDifferencePlotOnline<-function(plot, filename=NULL){
-  response = api_create(
-    x = plot, 
-    filename = filename, fileopt = "overwrite", 
-    sharing = "public");
-  response$web_url;
+computeColorMap<-function(numShades=20){
+  colorMap = colormap(colormap=colormaps$portland, nshades=numShades, reverse=F);
+  return(colorMap);
 }
+
+computeColors<-function(values, colormap){
+  colorIdxes = round((values * (length(colormap) - 1)) + 1);
+  return(colormap[colorIdxes]);
+}
+
+computeLegendValues<-function(values, colormap){
+  numShades = length(colormap);
+  legendValues = seq(from=min(values), to=max(values), length.out = numShades);
+  legendStrings = format(legendValues, scientific = T, digits=3)
+  return(legendStrings);
+}
+
+scaleToUnitRange<-function(values){
+  oldMin = min(values);
+  oldMax = max(values);
+  oldRange = oldMax - oldMin;
+  newValues = (values - oldMin) / oldRange;
+  return(newValues);
+}
+
+publishDifferencePlotLocally<-function(plot, file_name, outfilepath){
+  # api_create(plot, filename=file_name, sharing='public')
+  # plotly_IMAGE(plot, format = "png", out_file = outfilepath)
+}
+
+data = readDataSet('/Users/laura/Desktop/grid.txt')$data;
+values_1 = readResults('/Users/laura/Desktop/values1.txt')$computedDensity;
+values_2 = readResults('/Users/laura/Desktop/values2.txt')$computedDensity;
+plot_title = 'test plot';
+
+createDifferencePlot(data, values_1, values_2, plot_title)
