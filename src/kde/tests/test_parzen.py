c869e0acd7dde5d499917fb097e11d7c5d6e08ce
Laura Baakman <laurabaakman@gmail.com>
2017-07-21 14:17:30 +0200

    Assume that a results object is returned by the estimate function of parzen.

--- src/kde/tests/test_parzen.py
+++ src/kde/tests/test_parzen.py
@@ -28,7 +28,7 @@ class TestParzenEstimator(TestCase):
         estimator = ParzenEstimator(dimension=2,
                                     bandwidth=4, kernel_class=Gaussian,
                                     estimator_implementation=_ParzenEstimator_Python)
-        actual = estimator.estimate(xi_s=xi_s, x_s=x_s)
+        actual = estimator.estimate(xi_s=xi_s, x_s=x_s).densities
         expected = np.array([0.0096947375, 0.0095360625])
         np.testing.assert_array_almost_equal(actual, expected)
 
@@ -37,7 +37,7 @@ class TestParzenEstimator(TestCase):
         x_s = np.array([[0, 0], [1 / 4.0, 1 / 2.0]])
         estimator = ParzenEstimator(dimension=2, kernel_class=Gaussian,
                                     estimator_implementation=_ParzenEstimator_Python)
-        actual = estimator.estimate(xi_s=xi_s, x_s=x_s)
+        actual = estimator.estimate(xi_s=xi_s, x_s=x_s).densities
         expected = np.array([0.15133033, 0.14270123])
         np.testing.assert_array_almost_equal(actual, expected)
 
@@ -46,7 +46,7 @@ class TestParzenEstimator(TestCase):
         x_s = np.array([[0, 0], [1 / 4.0, 1 / 2.0]])
         estimator = ParzenEstimator(dimension=2, kernel_class=Gaussian,
                                     estimator_implementation=_ParzenEstimator_Python)
-        actual = estimator.estimate(xi_s=xi_s, x_s=x_s, general_bandwidth=4)
+        actual = estimator.estimate(xi_s=xi_s, x_s=x_s, general_bandwidth=4).densities
         expected = np.array([0.0096947375, 0.0095360625])
         np.testing.assert_array_almost_equal(actual, expected)
 
@@ -56,7 +56,7 @@ class TestParzenEstimator(TestCase):
         estimator = ParzenEstimator(dimension=2,
                                     bandwidth=4, kernel_class=Gaussian,
                                     estimator_implementation=_ParzenEstimator_C)
-        actual = estimator.estimate(xi_s=xi_s, x_s=x_s)
+        actual = estimator.estimate(xi_s=xi_s, x_s=x_s).densities
         expected = np.array([0.0096947375, 0.0095360625])
         np.testing.assert_array_almost_equal(actual, expected)
 
@@ -72,7 +72,7 @@ class ParzenEstimatorImpAbstractTest(object):
         estimator = self._estimator_class(
             xi_s=xi_s, x_s=x_s, dimension=2,
             kernel=Gaussian(), general_bandwidth=4)
-        actual = estimator.estimate()
+        actual = estimator.estimate().densities
         expected = np.array([0.0096947375, 0.0095360625])
         np.testing.assert_array_almost_equal(actual, expected)
 
@@ -82,7 +82,7 @@ class ParzenEstimatorImpAbstractTest(object):
         estimator = self._estimator_class(
             xi_s=xi_s, x_s=x_s, dimension=2,
             kernel=TestKernel(), general_bandwidth=4)
-        actual = estimator.estimate()
+        actual = estimator.estimate().densities
         expected = np.array([3 / 384.0, 15 / 1536.0])
         np.testing.assert_array_almost_equal(actual, expected)
 
@@ -102,7 +102,7 @@ class ParzenEstimatorImpAbstractTest(object):
         estimator = self._estimator_class(
             xi_s=xi_s, x_s=x_s, dimension=2,
             kernel=Epanechnikov(), general_bandwidth=4)
-        actual = estimator.estimate()
+        actual = estimator.estimate().densities
         expected = np.array([
             0.007545933739,
             0.007636453113,
@@ -132,7 +132,9 @@ class Test_ParzenEstimator_Python(ParzenEstimatorImpAbstractTest, TestCase):
             xi_s=xi_s, x_s=x_s,
             dimension=2,
             kernel=TestKernel(), general_bandwidth=4)
-        actual = estimator._estimate_pattern(pattern=pattern, factor=factor)
+        actual = estimator._estimate_pattern(
+            pattern=pattern, factor=factor
+        ).densities
         expected = 15 / 1536.0
         self.assertAlmostEqual(actual, expected)
 
