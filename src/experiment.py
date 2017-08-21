fa9e39d5d85e356f3f32ec7d9db9ec64f004b93f
Laura Baakman <laurabaakman@gmail.com>
2017-08-21 15:01:33 +0200

    Enable the silverman sensitvity instead of breiman.

--- src/experiment.py
+++ src/experiment.py
@@ -23,8 +23,8 @@ else:
     print('No default paths supported for this system, use -i and -o')
 
 sensitivities = {
-    # 'silverman': lambda d: 0.5,
-    'breiman': lambda d: 1.0 / d
+    'silverman': lambda d: 0.5,
+    # 'breiman': lambda d: 1.0 / d
 }
 
 estimators = {
