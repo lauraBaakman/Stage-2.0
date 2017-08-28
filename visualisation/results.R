abe4afec1b47d273c0bc8cf5a96649ba479758d6
Laura Baakman <laurabaakman@gmail.com>
2017-08-28 15:59:51 +0200

    Compute sensible plot limits.

--- visualisation/results.R
+++ visualisation/results.R
@@ -51,8 +51,8 @@ plotResultOfMultipleDensityDataSet <-function(data, outputFile, distribution, li
   plot <- plot + geom_point(aes(x=trueDensity, y=computedDensity), size=0.7, colour=cols, shape=21, stroke=0.2);
   plot <- plot + geom_line(aes(x=trueDensity, y=trueDensity));
   plot <- plot + 	xlab('true density') + ylab('computed density');
-  plot <- plot + scale_x_continuous(labels = fancy_scientificLabels) + scale_y_continuous(labels = fancy_scientificLabels);
-  
+  plot <- plot + scale_x_continuous(labels = fancy_scientificLabels, limits = c(limits['xMin'], limits['xMax'])) 
+  plot <- plot + scale_y_continuous(labels = fancy_scientificLabels, limits = c(limits['yMin'], limits['yMax']));
   plot <- plot + ggtitle(sprintf("MSE = %.7e", computeMSE(data)));
   # print(plot)
   ggsave(
@@ -81,10 +81,13 @@ findPlotLimits <- function(data){
   maximum = -Inf;
   
   for(df in data){
-    minimum = min(minimum, df$trueDensity, df$computedDensity);
-    maximum = max(maximum, df$trueDensity, df$computedDensity);
+    minimum = min(minimum, df$computedDensity);
+    maximum = max(maximum, df$computedDensity);
   }
-  c(minimum = minimum, maximum = maximum);
+  
+  trueDensities = data[1][[1]]$trueDensity
+  c(xMin = min(trueDensities), xMax = max(trueDensities), 
+    yMin = minimum, yMax = maximum);
 }
 
 extractEstimatorSensitivityDataset <- function(file_name){
